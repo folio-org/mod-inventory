b2fe2a8b45d7aebf53c6c006458615fde6e08829
Marc Johnson <marc@k-int.co.uk>
2017-10-31 15:48:44 +0000

    Use maven for testing via okapi MODINV-20

--- test-via-okapi-gradle.sh
+++ test-via-okapi-gradle.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-tenant_id="test_tenant"
+#Requires Okapi to be running on localhost:9301
+
+tenant_id=test_tenant
 okapi_proxy_address="http://localhost:9130"
+inventory_direct_address=http://localhost:9603
+inventory_instance_id=localhost-9603
 #Needs to be the specific version of Inventory Storage you want to use for testing
-inventory_storage_module_id="mod-inventory-storage-5.1.0-SNAPSHOT"
+inventory_storage_module_id="mod-inventory-storage-6.0.0-SNAPSHOT"
 
 echo "Check if Okapi is contactable"
 curl -w '\n' -X GET -D -   \
@@ -21,11 +25,18 @@ curl -w '\n' -X POST -D - \
      -d "${activate_json}"  \
      "${okapi_proxy_address}/_/proxy/tenants/${tenant_id}/modules"
 
-echo "Run API tests"
+#generate the descriptors needed for registration (need to be in target, due to registration)
+mvn clean compile -Dmaven.test.skip=true -q
 
-gradle -Dokapi.address="${okapi_address}" clean cleanTest testStorageViaOkapi
+echo "Register inventory module"
+./register.sh ${inventory_direct_address} ${inventory_instance_id} ${tenant_id}
+
+gradle -Dokapi.address="${okapi_proxy_address}" clean cleanTest testApiViaOkapi
 
 test_results=$?
+
+echo "Unregister inventory module"
+./unregister.sh ${inventory_instance_id} ${tenant_id}
 
 echo "Deactivate inventory storage for ${tenant_id}"
 curl -X DELETE -D - -w '\n' "${okapi_proxy_address}/_/proxy/tenants/${tenant_id}/modules/${inventory_storage_module_id}"
