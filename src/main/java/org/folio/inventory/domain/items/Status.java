44fc5baa0b994607a10f7881d82ee817b25b86f5
Niels Erik G. Nielsen <nielserik@indexdata.com>
2019-02-19 20:58:07 +0100

    Fix property name typo and code smell MODINV-104

--- src/main/java/org/folio/inventory/domain/items/Status.java
+++ src/main/java/org/folio/inventory/domain/items/Status.java
@@ -15,26 +15,26 @@ public class Status {
   public static final String NAME_KEY = "name";
   public static final String DATE_KEY = "date";
 
-  private JsonObject status = new JsonObject();
+  private JsonObject json = new JsonObject();
 
   public Status (String name) {
-    status.put(NAME_KEY,name);
+    json.put(NAME_KEY,name);
   }
-  
+
   public Status (JsonObject status) {
-    this.status = status;    
+    this.json = status;
   }
-  
+
   public JsonObject getJson() {
-    return status;
+    return json;
   }
-  
+
   public String getName () {
-    return status.getString(NAME_KEY);
+    return json.getString(NAME_KEY);
   }
-  
+
   public String getDate () {
-    return status.getString(DATE_KEY);
+    return json.getString(DATE_KEY);
   }
-  
+
 }
