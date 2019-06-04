5c1b006e8002ab36cddb46ee3ecd04556cb1058e
Igor_Gorchakov <Igor_Gorchakov@epam.com>
2019-06-04 17:57:20 +0300

    Add more assertions into tests

--- src/test/java/api/InstancesApiExamples.java
+++ src/test/java/api/InstancesApiExamples.java
@@ -265,6 +265,9 @@ public class InstancesApiExamples extends ApiTests {
 
     // Assertions
     assertThat(postResponse.getStatusCode(), is(HttpResponseStatus.INTERNAL_SERVER_ERROR.code()));
+    assertEquals(postResponse.getJson().getJsonArray("instances").size(), 0);
+    assertEquals(postResponse.getJson().getJsonArray("errorMessages").size(), 1);
+    assertEquals(postResponse.getJson().getInteger("totalRecords"), Integer.valueOf(0));
   }
 
   @Test
