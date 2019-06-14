39876381d0fbb5a5104631f8d5e10f24c62eb6e3
Igor_Gorchakov <Igor_Gorchakov@epam.com>
2019-06-14 16:57:53 +0300

    Fixed code smells

--- src/main/java/org/folio/inventory/resources/InventoryConfiguration.java
+++ src/main/java/org/folio/inventory/resources/InventoryConfiguration.java
@@ -1,7 +1,6 @@
 package org.folio.inventory.resources;
 
 import com.google.common.collect.Sets;
-
 import java.util.Set;
 
 /**
@@ -16,7 +15,6 @@ public class InventoryConfiguration {
     "previouslyHeld",
     "statusId",
     "clickable-add-statistical-code");
-
 
   private InventoryConfiguration() {
     throw new UnsupportedOperationException("Instance of the " + this.getClass().getName() + " + can not be created, because it is utility class");
