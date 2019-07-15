063558a55bb48e8a80abbfeb1f8a970630b720e1
kostyantyn-kh <43403459+kostyantyn-kh@users.noreply.github.com>
2019-07-15 12:28:42 +0300

    Update src/main/java/org/folio/inventory/resources/Items.java

--- src/main/java/org/folio/inventory/resources/Items.java
+++ src/main/java/org/folio/inventory/resources/Items.java
@@ -173,7 +173,7 @@ public class Items {
 
     itemCollection.findById(routingContext.request().getParam("id"), getItemResult -> {
       Item oldItem = getItemResult.getResult();
-      if(oldItem != null) {
+      if (oldItem != null) {
         if(hasSameBarcode(newItem, oldItem)) {
           findUserAndUpdateItem(routingContext, newItem, oldItem, userCollection, itemCollection);
         } else {
