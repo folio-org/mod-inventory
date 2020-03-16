77b631438b7d1263cb9e94e6fd9d7065800062a7
VRohach <Volodymyr_Rohach@epam.com>
2020-03-16 13:11:18 +0200

    MODINV-183: Implement action handler for Create Holding action. 1. New tests added.

---
+++ src/test/java/org/folio/inventory/storage/external/ExternalStorageModuleHoldingsRecordCollectionTest.java
@@ -0,0 +1,75 @@
+package org.folio.inventory.storage.external;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.UUID;
+
+import org.folio.Holdingsrecord;
+import org.folio.inventory.validation.exceptions.JsonMappingException;
+import org.junit.Before;
+import org.junit.Test;
+
+import io.vertx.core.json.JsonObject;
+
+public class ExternalStorageModuleHoldingsRecordCollectionTest {
+
+  private ExternalStorageModuleHoldingsRecordCollection storage;
+
+  @Before
+  public void before() {
+    storage = new ExternalStorageModuleHoldingsRecordCollection(null, null, null, null, null);
+  }
+
+  @Test
+  public void shouldMapFromJson() {
+    String holdingId = UUID.randomUUID().toString();
+    String instanceId = UUID.randomUUID().toString();
+    String permanentLocationId = UUID.randomUUID().toString();
+    JsonObject holdingsRecord = new JsonObject()
+      .put("id", holdingId)
+      .put("instanceId", instanceId)
+      .put("permanentLocationId", permanentLocationId);
+
+    Holdingsrecord holdingsrecord = storage.mapFromJson(holdingsRecord);
+    assertNotNull(holdingsRecord);
+    assertEquals(holdingId, holdingsRecord.getString("id"));
+    assertEquals(instanceId, holdingsRecord.getString("instanceId"));
+    assertEquals(permanentLocationId, holdingsRecord.getString("permanentLocationId"));
+  }
+
+  @Test(expected = JsonMappingException.class)
+  public void shouldNotMapFromJsonAndThrowException() {
+    JsonObject holdingsRecord = new JsonObject()
+      .put("testField", "testValue");
+
+    storage.mapFromJson(holdingsRecord);
+  }
+
+  @Test
+  public void shouldRetrieveId() {
+    String holdingId = UUID.randomUUID().toString();
+    Holdingsrecord holdingsrecord = new Holdingsrecord()
+      .withId(holdingId);
+    assertEquals(holdingId, storage.getId(holdingsrecord));
+  }
+
+  @Test
+  public void shouldMapToRequest() {
+    String holdingId = UUID.randomUUID().toString();
+    String instanceId = UUID.randomUUID().toString();
+    String permanentLocationId = UUID.randomUUID().toString();
+    Holdingsrecord holdingsrecord = new Holdingsrecord()
+      .withId(holdingId)
+      .withInstanceId(instanceId)
+      .withPermanentLocationId(permanentLocationId);
+
+    JsonObject jsonObject = storage.mapToRequest(holdingsrecord);
+    assertNotNull(jsonObject);
+    assertEquals(holdingId, jsonObject.getString("id"));
+    assertEquals(instanceId, jsonObject.getString("instanceId"));
+    assertEquals(permanentLocationId, jsonObject.getString("permanentLocationId"));
+  }
+
+
+}
