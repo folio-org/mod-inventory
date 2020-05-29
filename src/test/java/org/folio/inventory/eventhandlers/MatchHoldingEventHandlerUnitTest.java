f08c3265ef86f5f069ed670d64ca0c98527893d1
Kateryna Senchenko <kateryna_senchenko@epam.com>
2020-05-29 16:57:22 +0300

    Added support of submatch

--- src/test/java/org/folio/inventory/eventhandlers/MatchHoldingEventHandlerUnitTest.java
+++ src/test/java/org/folio/inventory/eventhandlers/MatchHoldingEventHandlerUnitTest.java
@@ -5,6 +5,7 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
+import java.util.Map;
 import org.folio.DataImportEventPayload;
 import org.folio.HoldingsRecord;
 import org.folio.MatchDetail;
@@ -25,6 +26,7 @@ import org.folio.processing.matching.reader.MarcValueReaderImpl;
 import org.folio.processing.matching.reader.MatchValueReaderFactory;
 import org.folio.processing.value.MissingValue;
 import org.folio.processing.value.StringValue;
+import org.folio.rest.jaxrs.model.EntityType;
 import org.folio.rest.jaxrs.model.Field;
 import org.folio.rest.jaxrs.model.MatchExpression;
 import org.folio.rest.jaxrs.model.ProfileSnapshotWrapper;
@@ -40,7 +42,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
 import java.util.function.Consumer;
+import sun.security.krb5.internal.crypto.EType;
 
+import static java.lang.String.format;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static org.folio.DataImportEventTypes.DI_INVENTORY_HOLDING_MATCHED;
@@ -57,6 +61,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
@@ -64,7 +69,8 @@ import static org.mockito.Mockito.when;
 @RunWith(VertxUnitRunner.class)
 public class MatchHoldingEventHandlerUnitTest {
 
-  private static final String HOLDING_ID = "001234";
+  private static final String HOLDING_ID = UUID.randomUUID().toString();
+  private static final String HOLDING_HRID = "1234";
 
   @Mock
   private Storage storage;
@@ -83,7 +89,7 @@ public class MatchHoldingEventHandlerUnitTest {
     when(marcValueReader.isEligibleForEntityType(MARC_BIBLIOGRAPHIC)).thenReturn(true);
     when(storage.getHoldingsRecordCollection(any(Context.class))).thenReturn(holdingCollection);
     when(marcValueReader.read(any(DataImportEventPayload.class), any(MatchDetail.class)))
-      .thenReturn(StringValue.of(HOLDING_ID));
+      .thenReturn(StringValue.of(HOLDING_HRID));
     MatchValueReaderFactory.register(marcValueReader);
     MatchValueLoaderFactory.register(holdingLoader);
   }
@@ -100,7 +106,7 @@ public class MatchHoldingEventHandlerUnitTest {
       callback.accept(result);
       return null;
     }).when(holdingCollection)
-      .findByCql(anyString(), any(PagingParameters.class), any(Consumer.class), any(Consumer.class));
+      .findByCql(eq(format("hrid == \"%s\"", HOLDING_HRID)), any(PagingParameters.class), any(Consumer.class), any(Consumer.class));
 
     EventHandler eventHandler = new MatchHoldingEventHandler();
     DataImportEventPayload eventPayload = createEventPayload();
@@ -287,12 +293,12 @@ public class MatchHoldingEventHandlerUnitTest {
             .withExistingMatchExpression(new MatchExpression()
               .withDataValueType(VALUE_FROM_RECORD)
               .withFields(singletonList(
-                new Field().withLabel("field").withValue("item.id"))
+                new Field().withLabel("field").withValue("holdings.hrid"))
               ))))));
   }
 
   private HoldingsRecord createHolding() {
-    return new HoldingsRecord().withId(HOLDING_ID);
+    return new HoldingsRecord().withId(HOLDING_ID).withHrid(HOLDING_HRID);
   }
 
 }
