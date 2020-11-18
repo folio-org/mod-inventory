bc67f39d1a5923f647837c7604ac601843213fb9
Volodymyr Rohach <Volodymyr_Rohach@epam.com>
2020-11-18 17:58:56 +0300

    MODINV-367: Repeatable field actions not working for Instances[BUGFIX] (#287)
    * Update NEWS
    
    * MODDINV-367: Repeatable field actions not working for Instances
    1. Mechanism for merging only NOT controlled fields added.
    
    * MODDINV-346: Problem with the repeatable check in/out notes field mapping actions
    1. Test added.
    
    Co-authored-by: Kateryna Senchenko <Kateryna_Senchenko@epam.com>

--- src/main/java/org/folio/inventory/support/InstanceUtil.java
+++ src/main/java/org/folio/inventory/support/InstanceUtil.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.folio.ChildInstance;
+import org.folio.ParentInstance;
 import org.folio.inventory.domain.instances.AlternativeTitle;
 import org.folio.inventory.domain.instances.Classification;
 import org.folio.inventory.domain.instances.Contributor;
@@ -16,9 +18,17 @@ import org.folio.inventory.domain.instances.Publication;
 import org.folio.inventory.domain.instances.titles.PrecedingSucceedingTitle;
 import org.folio.inventory.domain.sharedproperties.ElectronicAccess;
 
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
 public class InstanceUtil {
+
+  private static final String STATISTICAL_CODE_IDS_PROPERTY = "statisticalCodeIds";
+  private static final String NATURE_OF_CONTENT_TERM_IDS_PROPERTY = "natureOfContentTermIds";
+  private static final String PARENT_INSTANCES_PROPERTY = "parentInstances";
+  private static final String CHILDREN_INSTANCES_PROPERTY = "childInstances";
+
+
 
   private InstanceUtil() {
 
@@ -147,5 +157,76 @@ public class InstanceUtil {
     return source.containsKey(propertyName)
       ? JsonArrayHelper.toListOfStrings(source.getJsonArray(propertyName))
       : new ArrayList<>();
+  }
+
+  /**
+   * Merges fields from Instances which are NOT controlled by the underlying SRS MARC
+   * @param existing - Instance in DB
+   * @param mapped - Instance after mapping
+   * @return - result Instance
+   */
+  public static Instance mergeFieldsWhichAreNotControlled(Instance existing, org.folio.Instance mapped) {
+
+    mapped.setId(existing.getId());
+
+    List<ParentInstance> parentInstances = constructParentInstancesList(existing);
+
+    List<ChildInstance> childInstances = constructChildInstancesList(existing);
+
+    //Fields which are not affects by default mapping.
+    org.folio.Instance tmp = new org.folio.Instance()
+      .withId(existing.getId())
+      .withDiscoverySuppress(existing.getDiscoverySuppress())
+      .withStaffSuppress(existing.getStaffSuppress())
+      .withPreviouslyHeld(existing.getPreviouslyHeld())
+      .withCatalogedDate(existing.getCatalogedDate())
+      .withStatusId(existing.getStatusId())
+      .withStatisticalCodeIds(existing.getStatisticalCodeIds())
+      .withNatureOfContentTermIds(existing.getNatureOfContentTermIds())
+      .withParentInstances(parentInstances)
+      .withChildInstances(childInstances);
+
+    JsonObject existingInstanceAsJson = JsonObject.mapFrom(tmp);
+    JsonObject mappedInstanceAsJson = JsonObject.mapFrom(mapped);
+    JsonObject mergedInstanceAsJson = InstanceUtil.mergeInstances(existingInstanceAsJson, mappedInstanceAsJson);
+    return InstanceUtil.jsonToInstance(mergedInstanceAsJson);
+  }
+
+  private static List<ParentInstance> constructParentInstancesList(Instance existing) {
+    List<ParentInstance> parentInstances = new ArrayList<>();
+    for (InstanceRelationshipToParent parent : existing.getParentInstances()) {
+      ParentInstance parentInstance = new ParentInstance()
+        .withId(parent.getId())
+        .withSuperInstanceId(parent.getSuperInstanceId())
+        .withInstanceRelationshipTypeId(parent.getInstanceRelationshipTypeId());
+      parentInstances.add(parentInstance);
+    }
+    return parentInstances;
+  }
+
+  private static List<ChildInstance> constructChildInstancesList(Instance existing) {
+    List<ChildInstance> childInstances = new ArrayList<>();
+    for (InstanceRelationshipToChild child : existing.getChildInstances()) {
+      ChildInstance childInstance = new ChildInstance()
+        .withId(child.getId())
+        .withSubInstanceId(child.getSubInstanceId())
+        .withInstanceRelationshipTypeId(child.getInstanceRelationshipTypeId());
+      childInstances.add(childInstance);
+    }
+    return childInstances;
+  }
+
+  public static JsonObject mergeInstances(JsonObject existing, JsonObject mapped) {
+    //Statistical code, nature of content terms, parent/childInstances don`t revealed via mergeIn() because of simple array type.
+    JsonArray statisticalCodeIds = existing.getJsonArray(STATISTICAL_CODE_IDS_PROPERTY);
+    JsonArray natureOfContentTermIds = existing.getJsonArray(NATURE_OF_CONTENT_TERM_IDS_PROPERTY);
+    JsonArray parents = existing.getJsonArray(PARENT_INSTANCES_PROPERTY);
+    JsonArray children = existing.getJsonArray(CHILDREN_INSTANCES_PROPERTY);
+    JsonObject mergedInstanceAsJson = existing.mergeIn(mapped);
+    mergedInstanceAsJson.put(STATISTICAL_CODE_IDS_PROPERTY, statisticalCodeIds);
+    mergedInstanceAsJson.put(NATURE_OF_CONTENT_TERM_IDS_PROPERTY, natureOfContentTermIds);
+    mergedInstanceAsJson.put(PARENT_INSTANCES_PROPERTY, parents);
+    mergedInstanceAsJson.put(CHILDREN_INSTANCES_PROPERTY, children);
+    return mergedInstanceAsJson;
   }
 }
