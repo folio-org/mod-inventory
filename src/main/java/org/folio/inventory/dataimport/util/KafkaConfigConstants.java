1fe4379d38fc9175d2c9e2a4c62f7217709e55fa
Kateryna Senchenko <42532596+KaterynaSenchenko@users.noreply.github.com>
2021-02-02 13:22:02 +0200

    Fix names for required kafka variables (#299)

--- src/main/java/org/folio/inventory/dataimport/util/KafkaConfigConstants.java
+++ src/main/java/org/folio/inventory/dataimport/util/KafkaConfigConstants.java
@@ -2,11 +2,11 @@ package org.folio.inventory.dataimport.util;
 
 public final class KafkaConfigConstants {
 
-  public static final String KAFKA_HOST = "FOLIO_KAFKA_HOST";
-  public static final String KAFKA_PORT = "FOLIO_KAFKA_PORT";
+  public static final String KAFKA_HOST = "KAFKA_HOST";
+  public static final String KAFKA_PORT = "KAFKA_PORT";
   public static final String OKAPI_URL = "OKAPI_URL";
-  public static final String KAFKA_REPLICATION_FACTOR = "FOLIO_KAFKA_REPLICATION_FACTOR";
-  public static final String KAFKA_ENV = "FOLIO_KAFKA_ENV";
+  public static final String KAFKA_REPLICATION_FACTOR = "REPLICATION_FACTOR";
+  public static final String KAFKA_ENV = "ENV";
 
   private KafkaConfigConstants() {
   }
