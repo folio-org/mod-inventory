473c07a93e6fc0975bc80342fe2f5c690dffd651
Andrii Shapovalov <andrii_shapovalov@epam.com>
2021-02-05 15:24:09 +0200

    coding style issues fixed.

--- src/test/java/org/folio/inventory/validation/status/GenerateItemStatusTransitionsMatrix.java
+++ src/test/java/org/folio/inventory/validation/status/GenerateItemStatusTransitionsMatrix.java
@@ -1,10 +1,8 @@
 package org.folio.inventory.validation.status;
 
-import junitparams.JUnitParamsRunner;
 import org.folio.inventory.domain.items.ItemStatusName;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
@@ -14,7 +12,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RunWith(JUnitParamsRunner.class)
 public class GenerateItemStatusTransitionsMatrix {
   private static TargetItemStatusValidators targetItemStatusValidators;
 
@@ -27,16 +24,21 @@ public class GenerateItemStatusTransitionsMatrix {
   public void generate() {
     final String fieldDelim = "\t";
     final String stringDelim = "\"";
-    final List<String> sortedStatusNames = Arrays.stream(ItemStatusName.values()).map(itemStatusName -> itemStatusName.value()).sorted().collect(Collectors.toList());
+    final List<String> sortedStatusNames = Arrays.stream(ItemStatusName.values())
+      .map(itemStatusName -> itemStatusName.value())
+      .sorted()
+      .collect(Collectors.toList());
     final Date dateNow = new Date();
     StringBuilder sb = new StringBuilder();
-    sb.append("Generated at:"+dateNow);
+    // Create header
+    sb.append("Generated at:" + dateNow);
     sb.append(System.lineSeparator());
-    sb.append(stringDelim+"Initial\\Target"+stringDelim+fieldDelim);
-    sortedStatusNames.stream().forEach(statusName -> sb.append(stringDelim+statusName+stringDelim+fieldDelim));
+    sb.append(stringDelim + "Initial\\Target" + stringDelim + fieldDelim);
+    sortedStatusNames.stream().forEach(statusName -> sb.append(stringDelim + statusName + stringDelim + fieldDelim));
     sb.append(System.lineSeparator());
+    // Create body
     sortedStatusNames.stream().forEach(initStatusName -> {
-      sb.append(stringDelim+initStatusName+stringDelim+fieldDelim);
+      sb.append(stringDelim + initStatusName + stringDelim + fieldDelim);
       sortedStatusNames.stream().forEach(targetStatusName -> {
         AbstractTargetItemStatusValidator validator = targetItemStatusValidators.getValidator(ItemStatusName.forName(targetStatusName));
         sb.append(stringDelim);
@@ -46,10 +48,10 @@ public class GenerateItemStatusTransitionsMatrix {
       });
       sb.append(System.lineSeparator());
     });
-    System.out.println(sb.toString());
     try {
       String fileName = "./target/ItemStatusesAndTransitions.txt";
       System.out.println(fileName);
+      System.out.println(sb.toString());
       BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
       writer.write(sb.toString());
       writer.close();
