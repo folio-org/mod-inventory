ac4fa5b5772a7798d1ba226a7b861380bad7f7aa
VRohach <Volodymyr_Rohach@epam.com>
2022-01-27 17:59:03 +0200

    MODINV-619: Changed exception.

--- src/main/java/org/folio/inventory/dataimport/exceptions/OptimisticLockingException.java
+++ src/main/java/org/folio/inventory/dataimport/exceptions/OptimisticLockingException.java
@@ -3,7 +3,7 @@ package org.folio.inventory.dataimport.exceptions;
 /**
  * Exception for handling errors regarding Optimistic Locking mechanism
  */
-public class OptimisticLockingException extends Exception {
+public class OptimisticLockingException extends RuntimeException {
   public OptimisticLockingException(String message) {
     super(message);
   }
