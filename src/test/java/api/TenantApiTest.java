74638fd36cc7bf038574130a7b91f0ab0fc34bb5
AivarIUsupov <aivar_iusupov@epam.com>
2022-02-02 16:21:39 +0300

    MODINV-630 Add delete tenant endpoint for inventory module

--- src/test/java/api/TenantApiTest.java
+++ src/test/java/api/TenantApiTest.java
@@ -46,4 +46,50 @@ public class TenantApiTest extends ApiTests {
     PostgresConnectionOptions.setSystemProperties(systemProperties);
   }
 
+  @Test
+  public void shouldCreateAndDeleteSchema() throws Exception {
+    final var postCompleted = okapiClient
+      .post(ApiRoot.tenant(), "{}");
+
+    Response postResponse = postCompleted.toCompletableFuture().get(10, SECONDS);
+    assertThat(postResponse.getStatusCode(), is(200));
+
+    final var deleteCompleted = okapiClient
+      .delete(ApiRoot.tenant());
+
+    Response deleteResponse = deleteCompleted.toCompletableFuture().get(10, SECONDS);
+    assertThat(deleteResponse.getStatusCode(), is(200));
+  }
+
+  @Test
+  public void shouldNotDropSchemaWithIncorrectConnectionOptions() throws Exception {
+    final var postCompleted = okapiClient
+      .post(ApiRoot.tenant(), "{}");
+
+    Response postResponse = postCompleted.toCompletableFuture().get(10, SECONDS);
+    assertThat(postResponse.getStatusCode(), is(200));
+
+    PgConnectOptions pgConnectOptions = PostgresConnectionOptions.getConnectionOptions(TENANT_ID);
+    Map<String, String> systemProperties = Map.of(DB_HOST, pgConnectOptions.getHost(),
+      DB_DATABASE, pgConnectOptions.getDatabase(),
+      DB_PORT, String.valueOf(pgConnectOptions.getPort()),
+      DB_USERNAME, pgConnectOptions.getUser(),
+      DB_PASSWORD, pgConnectOptions.getPassword());
+    PostgresConnectionOptions.setSystemProperties(new HashMap<>());
+
+    final var deleteCompletedBefore = okapiClient
+      .delete(ApiRoot.tenant());
+
+    Response deleteResponseBefore = deleteCompletedBefore.toCompletableFuture().get(10, SECONDS);
+    assertThat(deleteResponseBefore.getStatusCode(), is(500));
+
+    PostgresConnectionOptions.setSystemProperties(systemProperties);
+
+    final var deleteCompletedAfter = okapiClient
+      .delete(ApiRoot.tenant());
+
+    Response deleteResponseAfter = deleteCompletedAfter.toCompletableFuture().get(10, SECONDS);
+    assertThat(deleteResponseAfter.getStatusCode(), is(200));
+  }
+
 }
