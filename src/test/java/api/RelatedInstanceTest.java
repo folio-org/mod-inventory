b520db5ec7ce1ba65fcdfa7e46a0f74e0e3bdbb4
William Welling <8352733+wwelling@users.noreply.github.com>
2022-04-27 11:17:44 -0500

    separate unit from integration tests

--- src/test/java/api/RelatedInstanceTest.java
+++ src/test/java/api/RelatedInstanceTest.java
@@ -24,42 +24,6 @@ import lombok.SneakyThrows;
 public class RelatedInstanceTest extends ApiTests {
 
   @Test
-  public void testRelatedInstanceFrom() {
-    String id = UUID.randomUUID().toString();
-    String instanceId = UUID.randomUUID().toString();
-    String relatedInstanceId = UUID.randomUUID().toString();
-    String relatedInstanceTypeId = UUID.randomUUID().toString();
-    RelatedInstance relatedInstance = new RelatedInstance(
-        id, instanceId, relatedInstanceId, relatedInstanceTypeId);
-
-    JsonObject relatedInstanceJson = mapFrom(relatedInstance);
-
-    RelatedInstance sameRelatedInstance = RelatedInstance.from(relatedInstanceJson, instanceId);
-
-    assertEquals(id, sameRelatedInstance.id);
-    assertEquals(instanceId, sameRelatedInstance.instanceId);
-    assertEquals(relatedInstanceId, sameRelatedInstance.relatedInstanceId);
-    assertEquals(relatedInstanceTypeId, sameRelatedInstance.relatedInstanceTypeId);
-
-    assertEquals(relatedInstance.id, sameRelatedInstance.id);
-    assertEquals(relatedInstance.instanceId, sameRelatedInstance.instanceId);
-    assertEquals(relatedInstance.relatedInstanceId, sameRelatedInstance.relatedInstanceId);
-    assertEquals(relatedInstance.relatedInstanceTypeId, sameRelatedInstance.relatedInstanceTypeId);
-
-    RelatedInstance inverseRelatedInstance = RelatedInstance.from(relatedInstanceJson, relatedInstanceId);
-
-    assertEquals(id, inverseRelatedInstance.id);
-    assertEquals(relatedInstanceId, inverseRelatedInstance.instanceId);
-    assertEquals(instanceId, inverseRelatedInstance.relatedInstanceId);
-    assertEquals(relatedInstanceTypeId, inverseRelatedInstance.relatedInstanceTypeId);
-
-    assertEquals(relatedInstance.id, inverseRelatedInstance.id);
-    assertEquals(relatedInstance.instanceId, inverseRelatedInstance.relatedInstanceId);
-    assertEquals(relatedInstance.relatedInstanceId, inverseRelatedInstance.instanceId);
-    assertEquals(relatedInstance.relatedInstanceTypeId, inverseRelatedInstance.relatedInstanceTypeId);
-  }
-
-  @Test
   @SneakyThrows
   public void canCreateInstanceWithRelatedInstances() {
     var instanceId = UUID.randomUUID();
