072a8d4a8952f9748ddc2252f46c8325f596daae
Igor Gorchakov <40858461+Igor-Gorchakov@users.noreply.github.com>
2022-06-06 11:03:40 +0300

    MODINV-705 Updating "MARC Authority" record (#522)

--- src/main/java/org/folio/inventory/dataimport/handlers/actions/UpdateAuthorityEventHandler.java
+++ src/main/java/org/folio/inventory/dataimport/handlers/actions/UpdateAuthorityEventHandler.java
@@ -1,7 +1,25 @@
 package org.folio.inventory.dataimport.handlers.actions;
 
-import static java.lang.String.format;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import org.apache.http.HttpStatus;
+import org.folio.ActionProfile;
+import org.folio.Authority;
+import org.folio.DataImportEventPayload;
+import org.folio.dbschema.ObjectMapperTool;
+import org.folio.inventory.common.domain.Failure;
+import org.folio.inventory.dataimport.cache.MappingMetadataCache;
+import org.folio.inventory.dataimport.exceptions.DataImportException;
+import org.folio.inventory.domain.AuthorityRecordCollection;
+import org.folio.inventory.storage.Storage;
+import org.folio.processing.events.services.publisher.KafkaEventPublisher;
+import org.folio.processing.exceptions.EventProcessingException;
+import org.folio.rest.jaxrs.model.ProfileSnapshotWrapper;
 
+import static java.lang.String.format;
 import static org.folio.ActionProfile.Action.UPDATE;
 import static org.folio.ActionProfile.FolioRecord.AUTHORITY;
 import static org.folio.ActionProfile.FolioRecord.MARC_AUTHORITY;
@@ -9,22 +27,15 @@ import static org.folio.DataImportEventTypes.DI_INVENTORY_AUTHORITY_UPDATED;
 import static org.folio.rest.jaxrs.model.DataImportEventTypes.DI_INVENTORY_AUTHORITY_UPDATED_READY_FOR_POST_PROCESSING;
 import static org.folio.rest.jaxrs.model.ProfileSnapshotWrapper.ContentType.MAPPING_PROFILE;
 
-import io.vertx.core.Future;
-import io.vertx.core.Promise;
-
-import org.folio.ActionProfile;
-import org.folio.Authority;
-import org.folio.DataImportEventPayload;
-import org.folio.inventory.dataimport.cache.MappingMetadataCache;
-import org.folio.inventory.dataimport.exceptions.DataImportException;
-import org.folio.inventory.domain.AuthorityRecordCollection;
-import org.folio.inventory.storage.Storage;
-import org.folio.processing.events.services.publisher.KafkaEventPublisher;
-import org.folio.rest.jaxrs.model.ProfileSnapshotWrapper;
-
 public class UpdateAuthorityEventHandler extends AbstractAuthorityEventHandler {
+  private static final int MAX_RETRIES_COUNT = Integer.parseInt(System.getenv().getOrDefault("inventory.di.ol.retry.number", "1"));
+  private static final String CURRENT_RETRY_NUMBER = "CURRENT_RETRY_NUMBER";
+  private static final String CURRENT_EVENT_TYPE_PROPERTY = "CURRENT_EVENT_TYPE";
+  private static final String CURRENT_AUTHORITY_PROPERTY = "CURRENT_AUTHORITY";
+  private static final String CURRENT_NODE_PROPERTY = "CURRENT_NODE";
   protected static final String ERROR_UPDATING_AUTHORITY_MSG_TEMPLATE = "Failed updating Authority. Cause: %s, status: '%s'";
   private final KafkaEventPublisher eventPublisher;
+
 
   public UpdateAuthorityEventHandler(Storage storage, MappingMetadataCache mappingMetadataCache,
                                      KafkaEventPublisher publisher) {
@@ -36,7 +47,7 @@ public class UpdateAuthorityEventHandler extends AbstractAuthorityEventHandler {
   protected Future<Authority> processAuthority(Authority authority,
                                                AuthorityRecordCollection authorityCollection,
                                                DataImportEventPayload payload) {
-    return updateAuthority(authority, authorityCollection);
+    return updateAuthority(payload, authority, authorityCollection);
   }
 
   @Override
@@ -79,12 +90,65 @@ public class UpdateAuthorityEventHandler extends AbstractAuthorityEventHandler {
     return DI_INVENTORY_AUTHORITY_UPDATED_READY_FOR_POST_PROCESSING.value();
   }
 
-  private Future<Authority> updateAuthority(Authority authority, AuthorityRecordCollection authorityRecordCollection) {
+  @Override
+  protected void prepareEvent(DataImportEventPayload payload) {
+    super.prepareEvent(payload);
+    payload.getContext().put(CURRENT_EVENT_TYPE_PROPERTY, payload.getEventType());
+    payload.getContext().put(CURRENT_NODE_PROPERTY, Json.encode(payload.getCurrentNode()));
+    payload.getContext().put(CURRENT_AUTHORITY_PROPERTY, Json.encode(payload.getContext().get(AUTHORITY.value())));
+  }
+
+  private Future<Authority> updateAuthority(DataImportEventPayload payload, Authority authority, AuthorityRecordCollection authorityRecordCollection) {
     Promise<Authority> promise = Promise.promise();
-    authorityRecordCollection.update(authority, success -> promise.complete(authority),
-      failure -> promise.fail(new DataImportException(format(ERROR_UPDATING_AUTHORITY_MSG_TEMPLATE, failure.getReason(),
-        failure.getStatusCode()))
-      ));
+    authorityRecordCollection.update(authority, success -> promise.complete(authority), failure -> {
+      if (failure.getStatusCode() == HttpStatus.SC_CONFLICT) {
+        processOLError(failure, payload, authority.getId(), authorityRecordCollection, promise);
+      } else {
+        promise.fail(new DataImportException(format(ERROR_UPDATING_AUTHORITY_MSG_TEMPLATE, failure.getReason(), failure.getStatusCode())));
+      }
+    });
     return promise.future();
+  }
+
+  private void processOLError(Failure failure, DataImportEventPayload payload, String recordId, AuthorityRecordCollection recordCollection, Promise<Authority> promise) {
+    int currentRetryNumber = payload.getContext().get(CURRENT_RETRY_NUMBER)  == null ? 0 : Integer.parseInt(payload.getContext().get(CURRENT_RETRY_NUMBER));
+    if (currentRetryNumber < MAX_RETRIES_COUNT) {
+      payload.getContext().put(CURRENT_RETRY_NUMBER, String.valueOf(currentRetryNumber + 1));
+      LOGGER.warn("Error updating Authority by id '{}' - '{}', status code '{}'. Retry UpdateAuthorityEventHandler handler...", recordId, failure.getReason(), failure.getStatusCode());
+      recordCollection.findById(recordId)
+        .thenAccept(actualRecord -> prepareDataAndReInvokeCurrentHandler(payload, promise, actualRecord))
+        .exceptionally(e -> {
+          payload.getContext().remove(CURRENT_RETRY_NUMBER);
+          String errMessage = format("Cannot get actual Authority by id: '%s' for jobExecutionId '%s'. Error: %s ", recordId, payload.getJobExecutionId(), e.getCause());
+          LOGGER.error(errMessage);
+          promise.fail(new EventProcessingException(errMessage));
+          return null;
+        });
+    } else {
+      payload.getContext().remove(CURRENT_RETRY_NUMBER);
+      String errMessage = format("Current retry number %s exceeded or equal given number %s for the Authority update for jobExecutionId '%s' ", MAX_RETRIES_COUNT, currentRetryNumber, payload.getJobExecutionId());
+      LOGGER.error(errMessage);
+      promise.fail(new EventProcessingException(errMessage));
+    }
+  }
+
+  private void prepareDataAndReInvokeCurrentHandler(DataImportEventPayload payload, Promise<Authority> promise, Authority actualAuthority) {
+    payload.getContext().put(AUTHORITY.value(), Json.encode(JsonObject.mapFrom(actualAuthority)));
+    payload.getEventsChain().remove(payload.getContext().get(CURRENT_EVENT_TYPE_PROPERTY));
+    try {
+      payload.setCurrentNode(ObjectMapperTool.getMapper().readValue(payload.getContext().get(CURRENT_NODE_PROPERTY), ProfileSnapshotWrapper.class));
+    } catch (JsonProcessingException e) {
+      LOGGER.error(format("Cannot map from CURRENT_NODE value %s", e.getCause()));
+    }
+    payload.getContext().remove(CURRENT_EVENT_TYPE_PROPERTY);
+    payload.getContext().remove(CURRENT_NODE_PROPERTY);
+    payload.getContext().remove(CURRENT_AUTHORITY_PROPERTY);
+    handle(payload).whenComplete((res, e) -> {
+      if (e != null) {
+        promise.fail(new EventProcessingException(e.getMessage()));
+      } else {
+        promise.complete(actualAuthority);
+      }
+    });
   }
 }
