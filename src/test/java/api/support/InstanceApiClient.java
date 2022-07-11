c12246f6be43d142468021860e829987dc9ce7f7
Volodymyr_Rohach <Volodymyr_Rohach@epam.com>
2022-07-11 16:16:52 +0300

    canAddTagToExistingInstanceWithUnconnectedPrecedingSucceeding - trying to improve test. Logs added[2].

--- src/test/java/api/support/InstanceApiClient.java
+++ src/test/java/api/support/InstanceApiClient.java
@@ -8,12 +8,17 @@ import java.net.MalformedURLException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
+import api.InstancesApiExamples;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.folio.inventory.support.http.client.OkapiHttpClient;
 import org.folio.inventory.support.http.client.Response;
 
 import io.vertx.core.json.JsonObject;
 
 public class InstanceApiClient {
+  private static final Logger LOGGER = LogManager.getLogger(InstanceApiClient.class);
+
   public static JsonObject createInstance(
     OkapiHttpClient client,
     JsonObject newInstanceRequest)
@@ -25,6 +30,9 @@ public class InstanceApiClient {
     final var postCompleted = client.post(ApiRoot.instances(), newInstanceRequest);
 
     Response postResponse = postCompleted.toCompletableFuture().get(5, SECONDS);
+
+    LOGGER.error("!!!! body:", postResponse.getBody());
+    LOGGER.error("!!!! json:", postResponse.getJson());
 
     assertThat("Failed to create instance",
       postResponse.getStatusCode(), is(201));
