1d355185e0637aeb755d5102ae2e4a9ca0a2572b
Marc Johnson <marc@k-int.co.uk>
2022-09-07 11:36:21 +0100

    Remove authority failure examples from suite MODINV-739

--- src/test/java/org/folio/inventory/storage/external/failure/ExternalAuthorityCollectionBadRequestExamples.java
+++ src/test/java/org/folio/inventory/storage/external/failure/ExternalAuthorityCollectionBadRequestExamples.java
@@ -1,22 +1,215 @@
 package org.folio.inventory.storage.external.failure;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.any;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+import org.folio.Authority;
+import org.folio.inventory.common.VertxAssistant;
+import org.folio.inventory.common.api.request.PagingParameters;
 import org.folio.inventory.common.domain.Failure;
+import org.folio.inventory.domain.AuthorityRecordCollection;
 import org.folio.inventory.storage.external.ExternalStorageCollections;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
-public class ExternalAuthorityCollectionBadRequestExamples extends ExternalAuthorityCollectionFailureExamples {
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.github.tomakehurst.wiremock.matching.UrlPathPattern;
 
-  public ExternalAuthorityCollectionBadRequestExamples() {
-    super(ExternalStorageFailureSuite.createUsing(
-        it -> new ExternalStorageCollections(
-                ExternalStorageFailureSuite.getBadRequestStorageAddress(), it.createHttpClient())));
+import lombok.SneakyThrows;
+
+class ExternalAuthorityCollectionBadRequestExamples {
+  private static final VertxAssistant vertxAssistant = new VertxAssistant();
+
+  @RegisterExtension
+  static WireMockExtension wireMockServer = WireMockExtension.newInstance()
+    .options(wireMockConfig().dynamicPort())
+    .build();
+
+  @BeforeAll
+  public static void beforeAll() {
+    vertxAssistant.start();
   }
 
-  @Override
-  protected void check(Failure failure) {
+  @AfterAll
+  public static void afterAll() {
+    vertxAssistant.stop();
+  }
+
+  @Test
+  @SneakyThrows
+  void serverErrorWhenCreatingAnAuthorityTriggersFailureCallback() {
+    wireMockServer.stubFor(any(collectionRoot())
+      .willReturn(badRequestResponse()));
+
+    AuthorityRecordCollection collection = createCollection();
+
+    CompletableFuture<Failure> failureCalled = new CompletableFuture<>();
+
+    collection.add(createAuthority(),
+      success -> fail("Completion callback should not be called"),
+      failureCalled::complete);
+
+    Failure failure = failureCalled.get(1000, TimeUnit.MILLISECONDS);
+
+    assertBadRequest(failure);
+  }
+
+  @Test
+  @SneakyThrows
+  void serverErrorWhenUpdatingAnAuthorityTriggersFailureCallback() {
+    wireMockServer.stubFor(any(individualItem())
+      .willReturn(badRequestResponse()));
+
+    AuthorityRecordCollection collection = createCollection();
+
+    CompletableFuture<Failure> failureCalled = new CompletableFuture<>();
+
+    collection.update(createAuthority(),
+      success -> fail("Completion callback should not be called"),
+      failureCalled::complete);
+
+    Failure failure = failureCalled.get(1000, TimeUnit.MILLISECONDS);
+
+    assertBadRequest(failure);
+  }
+
+  @Test
+  @SneakyThrows
+  void serverErrorWhenGettingAllAuthoritiesTriggersFailureCallback() {
+    wireMockServer.stubFor(any(collectionRoot())
+      .willReturn(badRequestResponse()));
+
+    AuthorityRecordCollection collection = createCollection();
+
+    CompletableFuture<Failure> failureCalled = new CompletableFuture<>();
+
+    collection.findAll(PagingParameters.defaults(),
+      success -> fail("Completion callback should not be called"),
+      failureCalled::complete);
+
+    Failure failure = failureCalled.get(1000, TimeUnit.MILLISECONDS);
+
+    assertBadRequest(failure);
+  }
+
+  @Test
+  @SneakyThrows
+  void serverErrorWhenGettingAnAuthorityRecordByIdTriggersFailureCallback() {
+    wireMockServer.stubFor(any(individualItem())
+      .willReturn(badRequestResponse()));
+
+    AuthorityRecordCollection collection = createCollection();
+
+    CompletableFuture<Failure> failureCalled = new CompletableFuture<>();
+
+    collection.findById(UUID.randomUUID().toString(),
+      success -> fail("Completion callback should not be called"),
+      failureCalled::complete);
+
+    Failure failure = failureCalled.get(1000, TimeUnit.MILLISECONDS);
+
+    assertBadRequest(failure);
+  }
+
+  @Test
+  @SneakyThrows
+  void serverErrorWhenDeletingAnAuthorityByIdTriggersFailureCallback() {
+    wireMockServer.stubFor(any(individualItem())
+      .willReturn(badRequestResponse()));
+
+    AuthorityRecordCollection collection = createCollection();
+
+    CompletableFuture<Failure> failureCalled = new CompletableFuture<>();
+
+    collection.delete(UUID.randomUUID().toString(),
+      success -> fail("Completion callback should not be called"),
+      failureCalled::complete);
+
+    Failure failure = failureCalled.get(1000, TimeUnit.MILLISECONDS);
+
+    assertBadRequest(failure);
+  }
+
+  @Test
+  @SneakyThrows
+  void serverErrorWhenDeletingAllAuthoritiesTriggersFailureCallback() {
+    wireMockServer.stubFor(any(collectionRoot())
+      .willReturn(badRequestResponse()));
+
+    AuthorityRecordCollection collection = createCollection();
+
+    CompletableFuture<Failure> failureCalled = new CompletableFuture<>();
+
+    collection.empty(
+      success -> fail("Completion callback should not be called"),
+      failureCalled::complete);
+
+    Failure failure = failureCalled.get(1000, TimeUnit.MILLISECONDS);
+
+    assertBadRequest(failure);
+  }
+
+  @Test
+  @SneakyThrows
+  void serverErrorWhenFindingAuthoritiesTriggersFailureCallback() {
+    wireMockServer.stubFor(any(collectionRoot())
+      .willReturn(badRequestResponse()));
+
+    AuthorityRecordCollection collection = createCollection();
+
+    CompletableFuture<Failure> failureCalled = new CompletableFuture<>();
+
+    collection.findByCql("title=\"*Small Angry*\"",
+      new PagingParameters(10, 0),
+      success -> fail("Completion callback should not be called"),
+      failureCalled::complete);
+
+    Failure failure = failureCalled.get(1000, TimeUnit.MILLISECONDS);
+
+    assertBadRequest(failure);
+  }
+
+  private Authority createAuthority() {
+    return new Authority()
+      .withId(UUID.randomUUID().toString());
+  }
+
+  private AuthorityRecordCollection createCollection() {
+    return vertxAssistant.createUsingVertx(
+      it -> new ExternalStorageCollections(
+        wireMockServer.baseUrl(), it.createHttpClient()))
+      .getAuthorityCollection("test_tenant", "");
+  }
+
+  private void assertBadRequest(Failure failure) {
     assertThat(failure.getReason(), is("Bad Request"));
     assertThat(failure.getStatusCode(), is(400));
+  }
+
+  private ResponseDefinitionBuilder badRequestResponse() {
+    return aResponse()
+      .withStatus(400)
+      .withBody("Bad Request")
+      .withHeader("Content-Type", "text/plain");
+  }
+
+  private UrlPathPattern collectionRoot() {
+    return urlPathMatching("/authority-storage/authorities");
+  }
+
+  private UrlPathPattern individualItem() {
+    return urlPathMatching("/authority-storage/authorities/[a-z0-9/-]*");
   }
 }
--- src/test/java/org/folio/inventory/storage/external/failure/ExternalAuthorityCollectionServerErrorExamples.java
+++ src/test/java/org/folio/inventory/storage/external/failure/ExternalAuthorityCollectionServerErrorExamples.java
@@ -1,22 +1,215 @@
 package org.folio.inventory.storage.external.failure;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.any;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+import org.folio.Authority;
+import org.folio.inventory.common.VertxAssistant;
+import org.folio.inventory.common.api.request.PagingParameters;
 import org.folio.inventory.common.domain.Failure;
+import org.folio.inventory.domain.AuthorityRecordCollection;
 import org.folio.inventory.storage.external.ExternalStorageCollections;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
-public class ExternalAuthorityCollectionServerErrorExamples extends ExternalAuthorityCollectionFailureExamples{
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.github.tomakehurst.wiremock.matching.UrlPathPattern;
 
-  public ExternalAuthorityCollectionServerErrorExamples() {
-    super(ExternalStorageFailureSuite.createUsing(
-        it -> new ExternalStorageCollections(
-                ExternalStorageFailureSuite.getServerErrorStorageAddress(), it.createHttpClient())));
+import lombok.SneakyThrows;
+
+class ExternalAuthorityCollectionServerErrorExamples {
+  private static final VertxAssistant vertxAssistant = new VertxAssistant();
+
+  @RegisterExtension
+  static WireMockExtension wireMockServer = WireMockExtension.newInstance()
+    .options(wireMockConfig().dynamicPort())
+    .build();
+
+  @BeforeAll
+  public static void beforeAll() {
+    vertxAssistant.start();
   }
 
-  @Override
-  protected void check(Failure failure) {
+  @AfterAll
+  public static void afterAll() {
+    vertxAssistant.stop();
+  }
+
+  @Test
+  @SneakyThrows
+  void serverErrorWhenCreatingAnAuthorityTriggersFailureCallback() {
+    wireMockServer.stubFor(any(collectionRoot())
+      .willReturn(serverErrorResponse()));
+
+    AuthorityRecordCollection collection = createCollection();
+
+    CompletableFuture<Failure> failureCalled = new CompletableFuture<>();
+
+    collection.add(createAuthority(),
+      success -> fail("Completion callback should not be called"),
+      failureCalled::complete);
+
+    Failure failure = failureCalled.get(1000, TimeUnit.MILLISECONDS);
+
+    assertServerError(failure);
+  }
+
+  @Test
+  @SneakyThrows
+  void serverErrorWhenUpdatingAnAuthorityTriggersFailureCallback() {
+    wireMockServer.stubFor(any(individualItem())
+      .willReturn(serverErrorResponse()));
+
+    AuthorityRecordCollection collection = createCollection();
+
+    CompletableFuture<Failure> failureCalled = new CompletableFuture<>();
+
+    collection.update(createAuthority(),
+      success -> fail("Completion callback should not be called"),
+      failureCalled::complete);
+
+    Failure failure = failureCalled.get(1000, TimeUnit.MILLISECONDS);
+
+    assertServerError(failure);
+  }
+
+  @Test
+  @SneakyThrows
+  void serverErrorWhenGettingAllAuthoritiesTriggersFailureCallback() {
+    wireMockServer.stubFor(any(collectionRoot())
+      .willReturn(serverErrorResponse()));
+
+    AuthorityRecordCollection collection = createCollection();
+
+    CompletableFuture<Failure> failureCalled = new CompletableFuture<>();
+
+    collection.findAll(PagingParameters.defaults(),
+      success -> fail("Completion callback should not be called"),
+      failureCalled::complete);
+
+    Failure failure = failureCalled.get(1000, TimeUnit.MILLISECONDS);
+
+    assertServerError(failure);
+  }
+
+  @Test
+  @SneakyThrows
+  void serverErrorWhenGettingAnAuthorityRecordByIdTriggersFailureCallback() {
+    wireMockServer.stubFor(any(individualItem())
+      .willReturn(serverErrorResponse()));
+
+    AuthorityRecordCollection collection = createCollection();
+
+    CompletableFuture<Failure> failureCalled = new CompletableFuture<>();
+
+    collection.findById(UUID.randomUUID().toString(),
+      success -> fail("Completion callback should not be called"),
+      failureCalled::complete);
+
+    Failure failure = failureCalled.get(1000, TimeUnit.MILLISECONDS);
+
+    assertServerError(failure);
+  }
+
+  @Test
+  @SneakyThrows
+  void serverErrorWhenDeletingAnAuthorityByIdTriggersFailureCallback() {
+    wireMockServer.stubFor(any(individualItem())
+      .willReturn(serverErrorResponse()));
+
+    AuthorityRecordCollection collection = createCollection();
+
+    CompletableFuture<Failure> failureCalled = new CompletableFuture<>();
+
+    collection.delete(UUID.randomUUID().toString(),
+      success -> fail("Completion callback should not be called"),
+      failureCalled::complete);
+
+    Failure failure = failureCalled.get(1000, TimeUnit.MILLISECONDS);
+
+    assertServerError(failure);
+  }
+
+  @Test
+  @SneakyThrows
+  void serverErrorWhenDeletingAllAuthoritiesTriggersFailureCallback() {
+    wireMockServer.stubFor(any(collectionRoot())
+      .willReturn(serverErrorResponse()));
+
+    AuthorityRecordCollection collection = createCollection();
+
+    CompletableFuture<Failure> failureCalled = new CompletableFuture<>();
+
+    collection.empty(
+      success -> fail("Completion callback should not be called"),
+      failureCalled::complete);
+
+    Failure failure = failureCalled.get(1000, TimeUnit.MILLISECONDS);
+
+    assertServerError(failure);
+  }
+
+  @Test
+  @SneakyThrows
+  void serverErrorWhenFindingAuthoritiesTriggersFailureCallback() {
+    wireMockServer.stubFor(any(collectionRoot())
+      .willReturn(serverErrorResponse()));
+
+    AuthorityRecordCollection collection = createCollection();
+
+    CompletableFuture<Failure> failureCalled = new CompletableFuture<>();
+
+    collection.findByCql("title=\"*Small Angry*\"",
+      new PagingParameters(10, 0),
+      success -> fail("Completion callback should not be called"),
+      failureCalled::complete);
+
+    Failure failure = failureCalled.get(1000, TimeUnit.MILLISECONDS);
+
+    assertServerError(failure);
+  }
+
+  private Authority createAuthority() {
+    return new Authority()
+      .withId(UUID.randomUUID().toString());
+  }
+
+  private AuthorityRecordCollection createCollection() {
+    return vertxAssistant.createUsingVertx(
+        it -> new ExternalStorageCollections(
+          wireMockServer.baseUrl(), it.createHttpClient()))
+      .getAuthorityCollection("test_tenant", "");
+  }
+
+  private void assertServerError(Failure failure) {
     assertThat(failure.getReason(), is("Server Error"));
     assertThat(failure.getStatusCode(), is(500));
+  }
+
+  private ResponseDefinitionBuilder serverErrorResponse() {
+    return aResponse()
+      .withStatus(500)
+      .withBody("Server Error")
+      .withHeader("Content-Type", "text/plain");
+  }
+
+  private UrlPathPattern collectionRoot() {
+    return urlPathMatching("/authority-storage/authorities");
+  }
+
+  private UrlPathPattern individualItem() {
+    return urlPathMatching("/authority-storage/authorities/[a-z0-9/-]*");
   }
 }
