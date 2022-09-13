c29e7405153f54d57b26981fc48bbae64a6ccb2e
Marc Johnson <marc@k-int.co.uk>
2022-09-13 12:17:31 +0100

    Use base class instead of suite for reference records client MODINV-739

--- src/test/java/org/folio/inventory/storage/external/ExternalStorageTests.java
+++ src/test/java/org/folio/inventory/storage/external/ExternalStorageTests.java
@@ -2,34 +2,20 @@ package org.folio.inventory.storage.external;
 
 import static api.ApiTestSuite.USER_ID;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import java.util.function.Function;
 
 import org.folio.inventory.common.VertxAssistant;
 import org.folio.inventory.support.http.client.OkapiHttpClient;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 
-import io.vertx.core.Vertx;
+import lombok.SneakyThrows;
 import support.fakes.FakeOkapi;
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
-  ExternalItemCollectionExamples.class,
-  ExternalInstanceCollectionExamples.class,
-  ExternalStorageModuleHoldingsRecordCollectionExamples.class,
-  ExternalStorageModuleAuthorityRecordCollectionExamples.class,
-  ExternalStorageModuleHoldingsRecordsSourceCollectionExamples.class
-})
-public class ExternalStorageSuite {
+public abstract class ExternalStorageTests {
   static final String TENANT_ID = "test_tenant";
   static final String TENANT_TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsInRlbmFudCI6ImRlbW9fdGVuYW50In0.29VPjLI6fLJzxQW0UhQ0jsvAn8xHz501zyXAxRflXfJ9wuDzT8TDf-V75PjzD7fe2kHjSV2dzRXbstt3BTtXIQ";
 
@@ -37,11 +23,7 @@ public class ExternalStorageSuite {
 
   private static String storageModuleDeploymentId;
 
-  public static <T> T useVertx(Function<Vertx, T> action) {
-    return vertxAssistant.createUsingVertx(action);
-  }
-
-  public static String getStorageAddress() {
+  protected static String getStorageAddress() {
     if (!useFakeStorageModule()) {
       return System.getProperty("inventory.storage.address");
     } else {
@@ -50,8 +32,8 @@ public class ExternalStorageSuite {
   }
 
   @BeforeClass
-  public static void beforeAll()
-    throws InterruptedException, ExecutionException, TimeoutException {
+  @SneakyThrows
+  public static void beforeAll() {
     vertxAssistant.start();
 
     if (useFakeStorageModule()) {
@@ -71,9 +53,8 @@ public class ExternalStorageSuite {
   }
 
   @AfterClass
-  public static void afterAll()
-    throws InterruptedException, ExecutionException, TimeoutException {
-
+  @SneakyThrows
+  public static void afterAll() {
     if (useFakeStorageModule()) {
       CompletableFuture<Void> undeployed = new CompletableFuture<>();
 
@@ -85,11 +66,9 @@ public class ExternalStorageSuite {
     vertxAssistant.stop();
   }
 
-  public static OkapiHttpClient createOkapiHttpClient()
-    throws MalformedURLException {
-
-    return new OkapiHttpClient(
-      vertxAssistant.getVertx(),
+  @SneakyThrows
+  protected static OkapiHttpClient createOkapiHttpClient() {
+    return new OkapiHttpClient(vertxAssistant.getVertx(),
       new URL(getStorageAddress()), TENANT_ID, TENANT_TOKEN, USER_ID, "1234",
       it -> System.out.printf("Request failed: %s%n", it.toString()));
   }
