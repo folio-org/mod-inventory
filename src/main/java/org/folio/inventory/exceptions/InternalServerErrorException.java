0f84d03a07bb36198d24d664e16c049c991ff2f3
pawelmol <pawel@mol.com.pl>
2023-05-10 15:05:41 +0200

    Fixed code smell: This class has 6 parents which is greater than 5 authorized.

--- src/main/java/org/folio/inventory/exceptions/InternalServerErrorException.java
+++ src/main/java/org/folio/inventory/exceptions/InternalServerErrorException.java
@@ -4,15 +4,15 @@ import org.apache.http.HttpStatus;
 import org.folio.inventory.common.domain.Failure;
 import org.folio.inventory.support.http.ContentType;
 
-public class InternalServerErrorException extends ExternalResourceFetchException {
+public class InternalServerErrorException extends AbstractInventoryException {
   public InternalServerErrorException(Throwable ex) {
-    super(ex.getMessage(), HttpStatus.SC_INTERNAL_SERVER_ERROR, ContentType.TEXT_PLAIN);
+    this(ex.getMessage());
   }
 
   public InternalServerErrorException(String reason) {
-    super(reason, HttpStatus.SC_INTERNAL_SERVER_ERROR, ContentType.TEXT_PLAIN);
+    super("Internal server exception: ", reason, HttpStatus.SC_INTERNAL_SERVER_ERROR, ContentType.TEXT_PLAIN);
   }
   public InternalServerErrorException(Failure failure){
-    super(failure.getReason(), failure.getStatusCode(), ContentType.TEXT_PLAIN);
+    super("Internal server exception: ", failure.getReason(), failure.getStatusCode(), ContentType.TEXT_PLAIN);
   }
 }
