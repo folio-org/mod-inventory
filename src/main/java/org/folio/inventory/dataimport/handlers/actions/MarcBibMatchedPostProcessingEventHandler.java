ae23eb470c92729ff80199d32cf8bb96830a2f47
Roman_Chernetskyi <Roman_Chernetskyi@epam.com>
2023-09-13 20:58:53 +0300

    [MODSOURCE-646] Lookup for instance at consortium if instance not present at local tenant during marcBib matched post processing

--- src/main/java/org/folio/inventory/dataimport/handlers/actions/MarcBibMatchedPostProcessingEventHandler.java
+++ src/main/java/org/folio/inventory/dataimport/handlers/actions/MarcBibMatchedPostProcessingEventHandler.java
@@ -8,9 +8,11 @@ import org.apache.logging.log4j.Logger;
 import org.folio.DataImportEventPayload;
 import org.folio.inventory.common.Context;
 import org.folio.inventory.common.api.request.PagingParameters;
+import org.folio.inventory.consortium.services.ConsortiumService;
 import org.folio.inventory.dataimport.handlers.matching.util.EventHandlingUtil;
 import org.folio.inventory.dataimport.util.ParsedRecordUtil;
 import org.folio.inventory.domain.HoldingsRecordCollection;
+import org.folio.inventory.domain.instances.Instance;
 import org.folio.inventory.domain.instances.InstanceCollection;
 import org.folio.inventory.storage.Storage;
 import org.folio.processing.events.services.handler.EventHandler;
@@ -20,6 +22,7 @@ import org.folio.rest.jaxrs.model.Record;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
 import static java.lang.String.format;
 import static java.util.Objects.isNull;
@@ -27,6 +30,7 @@ import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.folio.ActionProfile.FolioRecord.HOLDINGS;
 import static org.folio.ActionProfile.FolioRecord.INSTANCE;
 import static org.folio.DataImportEventTypes.DI_SRS_MARC_BIB_RECORD_MATCHED_READY_FOR_POST_PROCESSING;
+import static org.folio.inventory.dataimport.handlers.matching.util.EventHandlingUtil.constructContext;
 import static org.folio.inventory.dataimport.util.LoggerUtil.logParametersEventHandler;
 
 public class MarcBibMatchedPostProcessingEventHandler implements EventHandler {
@@ -39,9 +43,11 @@ public class MarcBibMatchedPostProcessingEventHandler implements EventHandler {
   private static final String ERROR_HOLDING_MSG = "Error loading inventory holdings for MARC BIB";
   private static final String MATCHED_MARC_BIB_KEY = "MATCHED_MARC_BIBLIOGRAPHIC";
   private final Storage storage;
+  private final ConsortiumService consortiumService;
 
-  public MarcBibMatchedPostProcessingEventHandler(Storage storage) {
+  public MarcBibMatchedPostProcessingEventHandler(Storage storage, ConsortiumService consortiumService) {
     this.storage = storage;
+    this.consortiumService = consortiumService;
   }
 
   @Override
@@ -72,6 +78,12 @@ public class MarcBibMatchedPostProcessingEventHandler implements EventHandler {
       }
 
       instanceCollection.findById(instanceId)
+        .thenCompose(localInstance -> {
+          if (localInstance == null) {
+            return getInstanceCentralTenantIfLocalTenantInConsortium(instanceId, context);
+          }
+          return CompletableFuture.completedFuture(localInstance);
+        })
         .whenComplete((v, t) -> {
           if (t == null && v != null) {
             dataImportEventPayload.getContext().put(INSTANCE.value(), Json.encode(v));
@@ -101,6 +113,18 @@ public class MarcBibMatchedPostProcessingEventHandler implements EventHandler {
       future.completeExceptionally(e);
     }
     return future;
+  }
+
+  private CompletionStage<Instance> getInstanceCentralTenantIfLocalTenantInConsortium(String instanceId, Context context) {
+    return consortiumService.getConsortiumConfiguration(context)
+      .toCompletionStage()
+      .thenCompose(consortiumConfigurationOptional -> {
+        if (consortiumConfigurationOptional.isPresent()) {
+          Context consortiumContext = constructContext(consortiumConfigurationOptional.get().getCentralTenantId(), context.getToken(), context.getOkapiLocation());
+          return storage.getInstanceCollection(consortiumContext).findById(instanceId);
+        }
+        return CompletableFuture.completedFuture(null);
+      });
   }
 
   @Override
