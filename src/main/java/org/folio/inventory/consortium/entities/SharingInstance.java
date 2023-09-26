cdccc4c9741776db23be070aaf872f91ed0f4a1a
RomanChernetskyi <111740564+RomanChernetskyi@users.noreply.github.com>
2023-09-26 15:36:35 +0300

    [MODSOURCE-646] Set central tenant to context at post processing during consortium flow (#621)

--- src/main/java/org/folio/inventory/consortium/entities/SharingInstance.java
+++ src/main/java/org/folio/inventory/consortium/entities/SharingInstance.java
@@ -1,5 +1,6 @@
 package org.folio.inventory.consortium.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
@@ -12,6 +13,7 @@ import java.util.UUID;
  * Entity that is used for sharing instance process
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonPropertyOrder({"id", "instanceIdentifier", "sourceTenantId", "targetTenantId", "status", "error"})
 public class SharingInstance {
 
