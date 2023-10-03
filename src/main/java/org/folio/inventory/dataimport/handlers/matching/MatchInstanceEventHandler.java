7a157fca7c649c591312e5ec881fb198f6934e7b
RomanChernetskyi <111740564+RomanChernetskyi@users.noreply.github.com>
2023-10-03 11:20:08 +0300

    [MODINV-868] Perform MARC To Instance Matching in Local Tenant & Central Tenant (#622)

--- src/main/java/org/folio/inventory/dataimport/handlers/matching/MatchInstanceEventHandler.java
+++ src/main/java/org/folio/inventory/dataimport/handlers/matching/MatchInstanceEventHandler.java
@@ -3,13 +3,14 @@ package org.folio.inventory.dataimport.handlers.matching;
 import static org.folio.DataImportEventTypes.DI_INVENTORY_INSTANCE_MATCHED;
 import static org.folio.DataImportEventTypes.DI_INVENTORY_INSTANCE_NOT_MATCHED;
 
+import org.folio.inventory.consortium.services.ConsortiumService;
 import org.folio.inventory.dataimport.cache.MappingMetadataCache;
 import org.folio.rest.jaxrs.model.EntityType;
 
 public class MatchInstanceEventHandler extends AbstractMatchEventHandler {
 
-  public MatchInstanceEventHandler(MappingMetadataCache mappingMetadataCache) {
-    super(mappingMetadataCache);
+  public MatchInstanceEventHandler(MappingMetadataCache mappingMetadataCache, ConsortiumService consortiumService) {
+    super(mappingMetadataCache, consortiumService);
   }
 
   @Override
@@ -25,5 +26,10 @@ public class MatchInstanceEventHandler extends AbstractMatchEventHandler {
   @Override
   protected String getNotMatchedEventType() {
     return DI_INVENTORY_INSTANCE_NOT_MATCHED.value();
+  }
+
+  @Override
+  protected boolean isConsortiumActionAvailable() {
+    return true;
   }
 }
