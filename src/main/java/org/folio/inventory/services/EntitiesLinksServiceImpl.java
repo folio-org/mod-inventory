e5c21747a9dda4eddf0facecb1d5d9a6f38f7ed6
Roman_Chernetskyi <Roman_Chernetskyi@epam.com>
2023-10-19 13:06:40 +0300

    [MODINV-890] Create entity links for sharing authorities on central tenant during Instance sharing process

--- src/main/java/org/folio/inventory/services/EntitiesLinksServiceImpl.java
+++ src/main/java/org/folio/inventory/services/EntitiesLinksServiceImpl.java
@@ -15,12 +15,12 @@ import org.folio.Link;
 import org.folio.LinkingRuleDto;
 import org.folio.inventory.common.Context;
 import org.folio.inventory.consortium.exceptions.ConsortiumException;
-import org.folio.inventory.support.http.client.Response;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
+import static java.lang.String.format;
 import static org.folio.inventory.consortium.util.ConsortiumUtil.DEFAULT_EXPIRATION_TIME_SECONDS;
 import static org.folio.inventory.consortium.util.ConsortiumUtil.EXPIRATION_TIME_PARAM;
 import static org.folio.inventory.consortium.util.ConsortiumUtil.createOkapiHttpClient;
@@ -29,7 +29,11 @@ public class EntitiesLinksServiceImpl implements EntitiesLinksService {
   private static final Logger LOGGER = LogManager.getLogger(EntitiesLinksServiceImpl.class);
   private static final String AUTHORITY_LINK_PATH = "/links/instances/%s";
   private static final String LINKING_RULES_PATH = "/linking-rules/instance-authority";
+  private static final String SUCCESS_MESSAGE = "Successfully %s entities links for instance with id: %s";
+  private static final String ERROR_MESSAGE = "Error during %s entities links for instance with id: %s, status code: %s, response message: %s";
   public static final String LINKS = "links";
+  public static final String GET = "get";
+  public static final String UPDATE = "update";
   private final HttpClient httpClient;
   private final AsyncCache<String, List<LinkingRuleDto>> linkingRulesCache;
 
@@ -46,38 +50,38 @@ public class EntitiesLinksServiceImpl implements EntitiesLinksService {
   public Future<List<Link>> getInstanceAuthorityLinks(Context context, String instanceId) {
     CompletableFuture<List<Link>> completableFuture = createOkapiHttpClient(context, httpClient)
       .thenCompose(client ->
-        client.get(context.getOkapiLocation() + String.format(AUTHORITY_LINK_PATH, instanceId))
+        client.get(context.getOkapiLocation() + format(AUTHORITY_LINK_PATH, instanceId))
           .thenCompose(httpResponse -> {
-            String successMessage = String.format("getInstanceAuthorityLinks:: Successfully retrieved entities links for instance with id: %s", instanceId);
-            String errorMessage = String.format("Error during retrieving entity links for instance with id: %s", instanceId);
-            return handleInstanceAuthorityLinksResponse(httpResponse, successMessage, errorMessage);
+            if (httpResponse.getStatusCode() == HttpStatus.SC_OK) {
+              List<Link> response = List.of(Json.decodeValue(Json.encode(httpResponse.getJson().getJsonArray(LINKS)), Link[].class));
+              LOGGER.debug(format(SUCCESS_MESSAGE, GET, instanceId));
+              return CompletableFuture.completedFuture(response);
+            } else {
+              String errorMessage = format(ERROR_MESSAGE, GET, instanceId, httpResponse.getStatusCode(), httpResponse.getBody());
+              LOGGER.warn(errorMessage, httpResponse.getBody());
+              return CompletableFuture.failedFuture(new ConsortiumException(errorMessage));
+            }
           }));
     return Future.fromCompletionStage(completableFuture);
   }
 
   @Override
-  public Future<List<Link>> putInstanceAuthorityLinks(Context context, String instanceId, List<Link> entityLinks) {
+  public Future<Void> putInstanceAuthorityLinks(Context context, String instanceId, List<Link> entityLinks) {
     JsonObject body = new JsonObject().put(LINKS, new JsonArray(entityLinks));
-    CompletableFuture<List<Link>> completableFuture = createOkapiHttpClient(context, httpClient)
+    CompletableFuture<Void> completableFuture = createOkapiHttpClient(context, httpClient)
       .thenCompose(client ->
-        client.put(context.getOkapiLocation() + String.format(AUTHORITY_LINK_PATH, instanceId), body)
+        client.put(context.getOkapiLocation() + format(AUTHORITY_LINK_PATH, instanceId), body)
           .thenCompose(httpResponse -> {
-            String successMessage = String.format("getInstanceAuthorityLinks:: Successfully updated entities links for instance with id: %s", instanceId);
-            String errorMessage = String.format("Error during updating entity links for instance with id: %s", instanceId);
-            return handleInstanceAuthorityLinksResponse(httpResponse, successMessage, errorMessage);
+            if (httpResponse.getStatusCode() == HttpStatus.SC_NO_CONTENT) {
+              LOGGER.debug(format(SUCCESS_MESSAGE, UPDATE, instanceId));
+              return CompletableFuture.completedFuture(null);
+            } else {
+              String errorMessage = format(ERROR_MESSAGE, UPDATE, instanceId, httpResponse.getStatusCode(), httpResponse.getBody());
+              LOGGER.warn(errorMessage, httpResponse.getBody());
+              return CompletableFuture.failedFuture(new ConsortiumException(errorMessage));
+            }
           }));
     return Future.fromCompletionStage(completableFuture);
-  }
-
-  private static CompletableFuture<List<Link>> handleInstanceAuthorityLinksResponse(Response httpResponse, String successMessage, String errorMessage) {
-    if (httpResponse.getStatusCode() == HttpStatus.SC_OK) {
-      List<Link> response = List.of(Json.decodeValue(Json.encode(httpResponse.getJson().getJsonArray(LINKS)), Link[].class));
-      LOGGER.debug(successMessage);
-      return CompletableFuture.completedFuture(response);
-    } else {
-      LOGGER.warn(errorMessage);
-      return CompletableFuture.failedFuture(new ConsortiumException(errorMessage));
-    }
   }
 
   @Override
@@ -97,8 +101,9 @@ public class EntitiesLinksServiceImpl implements EntitiesLinksService {
         LOGGER.debug("loadLinkingRules:: Successfully loaded linking rules for tenant with id: {}", context.getTenantId());
         return CompletableFuture.completedFuture(linkingRules);
       } else {
-        String message = String.format("Error during loading linking rules for tenant with id: %s", context.getTenantId());
-        LOGGER.warn(String.format("loadLinkingRules:: %s", message));
+        String message = format("Error during loading linking rules for tenant with id: %s, status code: %s, response message: %s",
+          context.getTenantId(), httpResponse.getStatusCode(), httpResponse.getBody());
+        LOGGER.warn(format("loadLinkingRules:: %s", message));
         return CompletableFuture.failedFuture(new ConsortiumException(message));
       }
     }));
