69383fc18772e1d2d2d79a6d243e8c0efd50d79d
Roman_Chernetskyi <Roman_Chernetskyi@epam.com>
2024-01-22 13:09:16 +0200

    Add NEWS

--- src/main/java/org/folio/inventory/exceptions/BadRequestException.java
+++ src/main/java/org/folio/inventory/exceptions/BadRequestException.java
@@ -3,6 +3,9 @@ package org.folio.inventory.exceptions;
 import org.apache.http.HttpStatus;
 import org.folio.inventory.support.http.ContentType;
 
+/**
+ * Exception for invalid data at request, 400 status code
+ */
 public class BadRequestException extends AbstractInventoryException {
   public BadRequestException(String message) {
     super("Bad request:", message, HttpStatus.SC_BAD_REQUEST, ContentType.TEXT_PLAIN);
