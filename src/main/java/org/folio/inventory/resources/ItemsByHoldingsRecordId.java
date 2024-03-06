ab96840f35524d2ead41849355473b9e413c386a
nielserik <nielserik@indexdata.com>
2024-03-06 13:04:48 +0100

    MODINV-839 style fix

--- src/main/java/org/folio/inventory/resources/ItemsByHoldingsRecordId.java
+++ src/main/java/org/folio/inventory/resources/ItemsByHoldingsRecordId.java
@@ -26,7 +26,12 @@ import java.io.UnsupportedEncodingException;
 import java.lang.invoke.MethodHandles;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
