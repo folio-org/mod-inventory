4be8d3eb9f074bd5d51be80f7634b9026bf3d698
Javokhir Abdullaev <101543142+JavokhirAbdullayev@users.noreply.github.com>
2024-04-16 21:13:24 +0500

    MODINV 949 - Fix OCLC (#713)
    * MODINV-1001 The sorting for Items on Instance details page is not worked
    
    * small fix
    
    * fix some issues and tests
    
    * small fix
    
    * fix test
    
    * move normalization before mapping
    
    * move normalization before mapping
    
    * move normalization before mapping
    
    * additional parameterized tests
    
    * using string manipulation approach
    
    * regex approach optimized
    
    * regex approach optimized

--- src/test/java/org/folio/inventory/dataimport/util/AdditionalFieldsUtilTest.java
+++ src/test/java/org/folio/inventory/dataimport/util/AdditionalFieldsUtilTest.java
@@ -352,43 +352,84 @@ public class AdditionalFieldsUtilTest {
     public String parsedContent;
 
     @Parameterized.Parameter(1)
-    public List<String> expectedOclcFields;
+    public String expectedParsedContent;
 
-    @Parameterized.Parameters(name = "{index}: parsedContent={0}, expectedOclcField={1}")
+    @Parameterized.Parameters(name = "{index}: parsedContent={0}, expectedParsedContent={1}")
     public static Collection<Object[]> data() {
       return Arrays.asList(new Object[][]{
-        {"{\"leader\":\"00120nam  22000731a 4500\",\"fields\":[{\"001\":\"in001\"}," +
-           "{\"035\":{\"subfields\":[{\"a\":\"(ybp7406411)in001\"}," +
-           "{\"a\":\"(OCoLC)000064758\"}," +
-           "{\"a\":\"(OCoLC)ocn00064758\"} ],\"ind1\":\" \",\"ind2\":\" \"}}," +
-           "{\"500\":{\"subfields\":[{\"a\":\"data\"}],\"ind1\":\" \",\"ind2\":\" \"}}]}",
-         List.of("(OCoLC)64758", "(ybp7406411)in001")},
-        {"{\"leader\":\"00120nam  22000731a 4500\",\"fields\":[{\"001\":\"in001\"}," +
-           "{\"035\":{\"subfields\":[{\"a\":\"(ybp7406411)in001\"}," +
-           "{\"a\":\"(OCoLC)tfe00064758\"} ],\"ind1\":\" \",\"ind2\":\" \"}}," +
-           "{\"500\":{\"subfields\":[{\"a\":\"data\"}],\"ind1\":\" \",\"ind2\":\" \"}}]}",
-         List.of("(OCoLC)tfe64758", "(ybp7406411)in001")},
-        {"{\"leader\":\"00120nam  22000731a 4500\",\"fields\":[{\"001\":\"in001\"}," +
-           "{\"035\":{\"subfields\":[{\"a\":\"(ybp7406411)in001\"}," +
-           "{\"a\":\"(OCoLC)00064758\"}," +
-           "{\"a\":\"(OCoLC)ocm00064758\"}," +
-           "{\"z\":\"(OCoLC)00024758\"} ],\"ind1\":\" \",\"ind2\":\" \"}}," +
-           "{\"500\":{\"subfields\":[{\"a\":\"data\"}],\"ind1\":\" \",\"ind2\":\" \"}}]}",
-         List.of("(OCoLC)64758", "(OCoLC)24758", "(ybp7406411)in001")
+        {
+          "{\"leader\":\"00120nam  22000731a 4500\",\"fields\":[{\"001\":\"in001\"}," +
+          "{\"035\":{\"subfields\":[{\"a\":\"(ybp7406411)in001\"}," +
+          "{\"a\":\"(OCoLC)00006475800\"}],\"ind1\":\" \",\"ind2\":\" \"}}," +
+          "{\"500\":{\"subfields\":[{\"a\":\"data\"}],\"ind1\":\" \",\"ind2\":\" \"}}]}",
+
+          "{\"leader\":\"00115nam  22000611a 4500\",\"fields\":[{\"001\":\"in001\"}," +
+          "{\"035\":{\"subfields\":[{\"a\":\"(ybp7406411)in001\"}," +
+          "{\"a\":\"(OCoLC)6475800\"}],\"ind1\":\" \",\"ind2\":\" \"}}," +
+          "{\"500\":{\"subfields\":[{\"a\":\"data\"}],\"ind1\":\" \",\"ind2\":\" \"}}]}"
+         },
+        {
+          "{\"leader\":\"00120nam  22000731a 4500\",\"fields\":[{\"001\":\"in001\"}," +
+          "{\"035\":{\"subfields\":[{\"a\":\"(ybp7406411)in001\"}," +
+          "{\"a\":\"(OCoLC)tfe0006475800\"} ],\"ind1\":\" \",\"ind2\":\" \"}}," +
+          "{\"500\":{\"subfields\":[{\"a\":\"data\"}],\"ind1\":\" \",\"ind2\":\" \"}}]}",
+
+          "{\"leader\":\"00118nam  22000611a 4500\",\"fields\":[{\"001\":\"in001\"}," +
+          "{\"035\":{\"subfields\":[{\"a\":\"(ybp7406411)in001\"}," +
+          "{\"a\":\"(OCoLC)tfe6475800\"}],\"ind1\":\" \",\"ind2\":\" \"}}," +
+          "{\"500\":{\"subfields\":[{\"a\":\"data\"}],\"ind1\":\" \",\"ind2\":\" \"}}]}"
         },
-        {"{\"leader\":\"00120nam  22000731a 4500\",\"fields\":[{\"001\":\"in001\"}," +
-           "{\"035\":{\"subfields\":[{\"a\":\"(OCoLC)00064758\"} ],\"ind1\":\" \",\"ind2\":\" \"}}," +
-           "{\"035\":{\"subfields\":[{\"a\":\"(OCoLC)ocn000064758\"} ],\"ind1\":\" \",\"ind2\":\" \"}}," +
-           "{\"035\":{\"subfields\":[{\"a\":\"(OCoLC)ocm0000064758\"}, {\"z\":\"(OCoLC)11114758\"} ],\"ind1\":\" \"," +
-           "\"ind2\":\" \"}}," +
-           "{\"500\":{\"subfields\":[{\"a\":\"data\"}],\"ind1\":\" \",\"ind2\":\" \"}}]}",
-         List.of("(OCoLC)64758", "(OCoLC)24758")
+        {
+          "{\"leader\":\"00120nam  22000731a 4500\",\"fields\":[{\"001\":\"in001\"}," +
+          "{\"035\":{\"subfields\":[{\"a\":\"(ybp7406411)in001\"}," +
+          "{\"a\":\"(OCoLC)00064758\"}," +
+          "{\"a\":\"(OCoLC)ocm00064758\"}," +
+          "{\"z\":\"(OCoLC)00024758\"} ],\"ind1\":\" \",\"ind2\":\" \"}}," +
+          "{\"500\":{\"subfields\":[{\"a\":\"data\"}],\"ind1\":\" \",\"ind2\":\" \"}}]}",
+
+          "{\"leader\":\"00127nam  22000611a 4500\",\"fields\":[{\"001\":\"in001\"}," +
+          "{\"035\":{\"subfields\":[{\"a\":\"(ybp7406411)in001\"}," +
+          "{\"a\":\"(OCoLC)64758\"},{\"z\":\"(OCoLC)24758\"}],\"ind1\":\" \",\"ind2\":\" \"}}," +
+          "{\"500\":{\"subfields\":[{\"a\":\"data\"}],\"ind1\":\" \",\"ind2\":\" \"}}]}"
+        },
+        {
+          "{\"leader\":\"00120nam  22000731a 4500\",\"fields\":[{\"001\":\"in001\"}," +
+          "{\"035\":{\"subfields\":[{\"a\":\"(OCoLC)00064758\"} ],\"ind1\":\" \",\"ind2\":\" \"}}," +
+          "{\"035\":{\"subfields\":[{\"a\":\"(OCoLC)ocn000064758\"} ],\"ind1\":\" \",\"ind2\":\" \"}}," +
+          "{\"035\":{\"subfields\":[{\"a\":\"(OCoLC)ocm0000064758\"}, {\"z\":\"(OCoLC)11114758\"} ],\"ind1\":\" \"," +
+          "\"ind2\":\" \"}}," +
+          "{\"500\":{\"subfields\":[{\"a\":\"data\"}],\"ind1\":\" \",\"ind2\":\" \"}}]}",
+
+          "{\"leader\":\"00111nam  22000611a 4500\",\"fields\":[{\"001\":\"in001\"}," +
+          "{\"035\":{\"subfields\":[{\"a\":\"(OCoLC)64758\"},{\"z\":\"(OCoLC)11114758\"}],\"ind1\":\" \",\"ind2\":\" \"}}," +
+          "{\"500\":{\"subfields\":[{\"a\":\"data\"}],\"ind1\":\" \",\"ind2\":\" \"}}]}"
+        },
+        {
+          "{\"leader\":\"00120nam  22000731a 4500\",\"fields\":[{\"001\":\"in001\"}," +
+          "{\"035\":{\"subfields\":[{\"a\":\"(ybp7406411)in001\"}],\"ind1\":\" \",\"ind2\":\" \"}}," +
+          "{\"035\":{\"subfields\":[{\"a\":\"(OCoLC)ocn00064758\"}],\"ind1\":\" \",\"ind2\":\" \"}}," +
+          "{\"035\":{\"subfields\":[{\"a\":\"(OCoLC)ocm000064758\"}],\"ind1\":\" \",\"ind2\":\" \"}}," +
+          "{\"500\":{\"subfields\":[{\"a\":\"data\"}],\"ind1\":\" \",\"ind2\":\" \"}}]}",
+
+          "{\"leader\":\"00128nam  22000731a 4500\",\"fields\":[{\"001\":\"in001\"}," +
+          "{\"035\":{\"subfields\":[{\"a\":\"(ybp7406411)in001\"}],\"ind1\":\" \",\"ind2\":\" \"}}," +
+          "{\"035\":{\"subfields\":[{\"a\":\"(OCoLC)64758\"}],\"ind1\":\" \",\"ind2\":\" \"}}," +
+          "{\"500\":{\"subfields\":[{\"a\":\"data\"}],\"ind1\":\" \",\"ind2\":\" \"}}]}"
+        },
+        {
+          "{\"leader\":\"00120nam  22000731a 4500\",\"fields\":[{\"001\":\"in001\"}," +
+          "{\"035\":{\"subfields\":[{\"a\":\"(OCoLC)ocn607TST001\"}],\"ind1\":\" \",\"ind2\":\" \"}}," +
+          "{\"500\":{\"subfields\":[{\"a\":\"data\"}],\"ind1\":\" \",\"ind2\":\" \"}}]}",
+
+          "{\"leader\":\"00098nam  22000611a 4500\",\"fields\":[{\"001\":\"in001\"}," +
+          "{\"035\":{\"subfields\":[{\"a\":\"(OCoLC)607TST001\"}],\"ind1\":\" \",\"ind2\":\" \"}}," +
+          "{\"500\":{\"subfields\":[{\"a\":\"data\"}],\"ind1\":\" \",\"ind2\":\" \"}}]}"
         }
       });
     }
 
     @Test
-    public void shouldNormalizeField035OCoLC() {
+    public void shouldNormalizeOCoLCField035() {
       // given
       ParsedRecord parsedRecord = new ParsedRecord().withContent(parsedContent);
 
@@ -399,40 +440,8 @@ public class AdditionalFieldsUtilTest {
         .withExternalIdsHolder(new ExternalIdsHolder().withInstanceId("001").withInstanceHrid("in001"));
       // when
       AdditionalFieldsUtil.normalize035(record);
-      var oclcSubfields = get035SubfieldOclcValues(record, TAG_035, TAG_035_SUB).stream().map(Subfield::getData)
-        .toList();
-
-      // then
-      Assert.assertTrue(oclcSubfields.contains(expectedOclcFields.get(0)));
-      Assert.assertEquals(oclcSubfields.size(), expectedOclcFields.size());
+      Assert.assertEquals(expectedParsedContent, parsedRecord.getContent());
     }
-  }
-
-
-  @Test
-  public void shouldReturnSubfieldIfOclc() {
-    // given
-    String parsedContent = "{\"leader\":\"00120nam  22000731a 4500\",\"fields\":[{\"001\":\"in001\"}," +
-      "{\"035\":{\"subfields\":[{\"a\":\"(ybp7406411)in001\"}],\"ind1\":\" \",\"ind2\":\" \"}}," +
-      "{\"035\":{\"subfields\":[{\"a\":\"(OCoLC)ocn00064758\"}],\"ind1\":\" \",\"ind2\":\" \"}}," +
-      "{\"035\":{\"subfields\":[{\"a\":\"(OCoLC)ocm000064758\"}],\"ind1\":\" \",\"ind2\":\" \"}}," +
-      "{\"500\":{\"subfields\":[{\"a\":\"data\"}],\"ind1\":\" \",\"ind2\":\" \"}}]}";
-    String expectedParsedContent = "{\"leader\":\"00128nam  22000731a 4500\",\"fields\":[{\"001\":\"in001\"}," +
-      "{\"035\":{\"subfields\":[{\"a\":\"(ybp7406411)in001\"}],\"ind1\":\" \",\"ind2\":\" \"}}," +
-      "{\"035\":{\"subfields\":[{\"a\":\"(OCoLC)64758\"}],\"ind1\":\" \",\"ind2\":\" \"}}," +
-      "{\"500\":{\"subfields\":[{\"a\":\"data\"}],\"ind1\":\" \",\"ind2\":\" \"}}]}";
-
-    ParsedRecord parsedRecord = new ParsedRecord().withContent(parsedContent);
-
-    Record record = new Record().withId(UUID.randomUUID().toString())
-      .withParsedRecord(parsedRecord)
-      .withGeneration(0)
-      .withState(Record.State.ACTUAL)
-      .withExternalIdsHolder(new ExternalIdsHolder().withInstanceId("001").withInstanceHrid("in001"));
-    // when
-    AdditionalFieldsUtil.normalize035(record);
-    // then
-    Assert.assertEquals(expectedParsedContent, parsedRecord.getContent());
   }
 
   @Test
