2019a172a7288b63254d570f1ce8ffa9e13f14e9
Volodymyr Rohach <Volodymyr_Rohach@epam.com>
2024-04-19 13:34:14 +0300

    MODINV-1012: Implementation for Validation for NatureContentTerms added for Instance. Tests added. (#715)
    * MODINV-1012: Implementation for Validation for NatureContentTerms added for Instance. Tests added.
    
    * MODINV-1012: Documentation added.
    
    * MODINV-1012: Documentation added.
    
    * MODINV-1012: Tests added.

---
+++ src/main/java/org/folio/inventory/dataimport/util/ValidationUtil.java
@@ -0,0 +1,48 @@
+package org.folio.inventory.dataimport.util;
+
+import org.folio.inventory.domain.instances.Instance;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Util for detailed validation different entities.
+ */
+public class ValidationUtil {
+
+  private ValidationUtil() {
+  }
+
+  /**
+   * Validate fields inside the Instance entity. Validation based on checking if specific fields were mapped as UUIDs.
+   * If not - then the list with errors will be returned.
+   * Example: "Value 'invalid not UUID value' is not a UUID for someFieldName field"
+   * @param instance target Instance for validation
+   * @return ArrayList with errors when the needed fields are NOT as UUID.
+   */
+  public static List<String> validateUUIDs(Instance instance) {
+    ArrayList<String> errorMessages = new ArrayList<>();
+
+    //TODO: This will be extended for different fields and entities.That's why there are so many methods just for 1 field.
+    // Branch for it extending validation: MODINV-1012-extended
+    validateField(errorMessages, instance.getNatureOfContentTermIds(), "natureOfContentTermIds");
+
+    return errorMessages;
+  }
+
+  private static void validateField(List<String> errorMessages, List<String> values, String fieldName) {
+    values.stream()
+      .filter(value -> !isUUID(value))
+      .forEach(value -> errorMessages.add(String.format("Value '%s' is not a UUID for %s field", value, fieldName)));
+  }
+
+  private static boolean isUUID(String value) {
+    try {
+      UUID.fromString(value);
+      return true;
+    } catch (Exception ex) {
+      return false;
+    }
+  }
+}
