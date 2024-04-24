510dc0bc095a2a7b166666a34f1a7d6dce15307a
pbobylev <pavel_bobylev@epam.com>
2024-04-24 15:17:52 +0500

    MODINV-986: instance_ingest -> instance_ingress

--- src/main/java/org/folio/inventory/handler/InstanceIngressEventHandler.java
+++ src/main/java/org/folio/inventory/handler/InstanceIngressEventHandler.java
@@ -6,15 +6,16 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.folio.kafka.AsyncRecordHandler;
 
-public class InstanceIngestEventHandler implements AsyncRecordHandler<String, String> {
+public class InstanceIngressEventHandler implements AsyncRecordHandler<String, String> {
 
-  private static final Logger LOGGER = LogManager.getLogger(InstanceIngestEventHandler.class);
+  private static final Logger LOGGER = LogManager.getLogger(
+    InstanceIngressEventHandler.class);
 
   @Override
   public Future<String> handle(KafkaConsumerRecord<String, String> kafkaConsumerRecord) {
     // to extract and re-use common logic from CreateInstanceEventHandler
     // 1. Change event; 2. Re-use all except: source type to be changed to BIBFRAME, DI event not to be sent
-    LOGGER.info("to be replaced with actual code in Step 2 of MODINV-986");
+
     return null;
   }
 
