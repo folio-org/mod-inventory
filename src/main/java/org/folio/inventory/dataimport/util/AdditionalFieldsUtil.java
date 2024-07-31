de0997a0ebb0dca293dbc93ac1c90ca6063bd260
Javokhir Abdullaev <101543142+JavokhirAbdullayev@users.noreply.github.com>
2024-07-31 14:34:01 +0500

    Modinv 1049 remove atomic integer (#749)
    * MODINV-1049 Existing "035" field is not retained the original position in imported record
    
    * MODINV-1049 Existing "035" field is not retained the original position in imported record

--- src/main/java/org/folio/inventory/dataimport/util/AdditionalFieldsUtil.java
+++ src/main/java/org/folio/inventory/dataimport/util/AdditionalFieldsUtil.java
@@ -69,7 +69,6 @@ public final class AdditionalFieldsUtil {
   public static final String TAG_999 = "999";
   public static final String TAG_001 = "001";
   private static final String TAG_003 = "003";
-  private static final String TAG_010 = "010";
   public static final String TAG_035 = "035";
   public static final char TAG_035_SUB = 'a';
   private static final char TAG_035_IND = ' ';
@@ -583,16 +582,15 @@ public final class AdditionalFieldsUtil {
   private static void replaceOclc035FieldWithNormalizedData(org.marc4j.marc.Record marcRecord,
                                               DataField dataField) {
     var variableFields = marcRecord.getVariableFields(TAG_035);
-    var index = new AtomicInteger();
+    int[] index = {0};
     if (!variableFields.isEmpty()) {
       variableFields.stream()
         .filter(variableField -> variableField.find(OCLC))
         .forEach(variableField -> {
-          if (index.get() == 0)
-            index.set(marcRecord.getDataFields().indexOf(variableField));
+          index[0] = (marcRecord.getDataFields().indexOf(variableField));
           marcRecord.removeVariableField(variableField);
         });
-      marcRecord.getDataFields().add(index.get(), dataField);
+      marcRecord.getDataFields().add(index[0], dataField);
     }
   }
 
