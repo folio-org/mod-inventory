cb329b454f1595de5af92888fee9fd79efc08c13
Roman_Chernetskyi <Roman_Chernetskyi@epam.com>
2024-08-19 15:53:37 +0300

    [MODINV-1060] Update ownership of holdings with new location

--- src/test/java/api/support/builders/HoldingsRecordUpdateOwnershipRequestBuilder.java
+++ src/test/java/api/support/builders/HoldingsRecordUpdateOwnershipRequestBuilder.java
@@ -11,15 +11,17 @@ import static org.folio.inventory.resources.MoveApi.TO_INSTANCE_ID;
 import static org.folio.inventory.support.MoveApiUtil.TARGET_TENANT_ID;
 
 public class HoldingsRecordUpdateOwnershipRequestBuilder extends AbstractBuilder {
-
+  private static final String TARGET_LOCATION_ID = "targetLocationId";
   private final UUID toInstanceId;
   private final JsonArray holdingsRecordsIds;
   private final String tenantId;
+  private final UUID targetLocationId;
 
-  public HoldingsRecordUpdateOwnershipRequestBuilder(UUID toInstanceId, JsonArray holdingsRecordsIds, String tenantId) {
+  public HoldingsRecordUpdateOwnershipRequestBuilder(UUID toInstanceId, JsonArray holdingsRecordsIds, UUID targetLocationId, String tenantId) {
     this.toInstanceId = toInstanceId;
     this.holdingsRecordsIds = holdingsRecordsIds;
     this.tenantId = tenantId;
+    this.targetLocationId = targetLocationId;
   }
 
   public JsonObject create() {
@@ -28,6 +30,7 @@ public class HoldingsRecordUpdateOwnershipRequestBuilder extends AbstractBuilder
     includeWhenPresent(holdingsRecordUpdateOwnershipRequest, TO_INSTANCE_ID, toInstanceId);
     includeWhenPresent(holdingsRecordUpdateOwnershipRequest, HOLDINGS_RECORD_IDS, holdingsRecordsIds);
     includeWhenPresent(holdingsRecordUpdateOwnershipRequest, TARGET_TENANT_ID, tenantId);
+    includeWhenPresent(holdingsRecordUpdateOwnershipRequest, TARGET_LOCATION_ID, targetLocationId);
 
     return holdingsRecordUpdateOwnershipRequest;
   }
