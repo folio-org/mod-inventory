baa667f5c45d78b6e8c0d44578c2814e72b7e451
Dmytro Krutii <dmytro_krutii@epam.com>
2024-12-10 16:27:52 +0200

    MODINV-1115 Change logs

--- src/main/java/org/folio/inventory/dataimport/handlers/actions/CreateInstanceEventHandler.java
+++ src/main/java/org/folio/inventory/dataimport/handlers/actions/CreateInstanceEventHandler.java
@@ -95,7 +95,7 @@ public class CreateInstanceEventHandler extends AbstractInstanceEventHandler {
       }
 
       Context context = EventHandlingUtil.constructContext(dataImportEventPayload.getTenant(), dataImportEventPayload.getToken(), dataImportEventPayload.getOkapiUrl());
-      LOGGER.info("CreateInstanceEventHandler.handle:: context: {} ", context);
+      LOGGER.info("CreateInstanceEventHandler.handle:: context token: {} ", context.getToken());
       Record targetRecord = Json.decodeValue(payloadContext.get(EntityType.MARC_BIBLIOGRAPHIC.value()), Record.class);
       var sourceContent = targetRecord.getParsedRecord().getContent().toString();
 
