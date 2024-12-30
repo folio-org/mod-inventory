1d76fb20d2a8f346f4956d97917a0df195a6371a
Ruslan Lavrov <47384893+RuslanLavrov@users.noreply.github.com>
2024-12-30 17:41:48 +0200

    MODINV-1100: Provide consistent handling of two or more Marc Bib Update events for the same Bib/Instance record (Quesnelia fix) (#796)
    ---------
    
    Co-authored-by: Mukhiddin Yusupov <mukhiddin_yusupov@epam.com>

--- src/main/java/org/folio/inventory/dataimport/handlers/actions/InstanceUpdateDelegate.java
+++ src/main/java/org/folio/inventory/dataimport/handlers/actions/InstanceUpdateDelegate.java
@@ -3,6 +3,7 @@ package org.folio.inventory.dataimport.handlers.actions;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.json.JsonObject;
+import java.util.Map;
 import org.apache.http.HttpStatus;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -18,10 +19,9 @@ import org.folio.processing.mapping.defaultmapper.processor.parameters.MappingPa
 import org.folio.rest.jaxrs.model.ParsedRecord;
 import org.folio.rest.jaxrs.model.Record;
 
-import java.util.Map;
-
 import static java.lang.String.format;
 import static org.folio.inventory.dataimport.util.LoggerUtil.logParametersUpdateDelegate;
+import static org.folio.inventory.dataimport.util.MappingConstants.MARC_BIB_RECORD_FORMAT;
 
 public class InstanceUpdateDelegate {
 
@@ -58,6 +58,26 @@ public class InstanceUpdateDelegate {
     } catch (Exception e) {
       LOGGER.error("Error updating inventory instance", e);
       return Future.failedFuture(e);
+    }
+  }
+
+  public Instance handleBlocking(Map<String, String> eventPayload, Record marcRecord, Context context) {
+    logParametersUpdateDelegate(LOGGER, eventPayload, marcRecord, context);
+    try {
+      JsonObject mappingRules = new JsonObject(eventPayload.get(MAPPING_RULES_KEY));
+      MappingParameters mappingParameters = new JsonObject(eventPayload.get(MAPPING_PARAMS_KEY)).mapTo(MappingParameters.class);
+      JsonObject parsedRecord = retrieveParsedContent(marcRecord.getParsedRecord());
+      String instanceId = marcRecord.getExternalIdsHolder().getInstanceId();
+      LOGGER.info("Instance update with instanceId: {}", instanceId);
+      RecordMapper<org.folio.Instance> recordMapper = RecordMapperBuilder.buildMapper(MARC_BIB_RECORD_FORMAT);
+      var mappedInstance = recordMapper.mapRecord(parsedRecord, mappingParameters, mappingRules);
+      var modified = JsonObject.mapFrom(mappedInstance);
+      InstanceCollection instanceCollection = storage.getInstanceCollection(context);
+
+      return instanceCollection.findByIdAndUpdate(instanceId, modified, context);
+    } catch (Exception ex) {
+      LOGGER.error("Error updating inventory instance: {}", ex.getMessage());
+      throw ex;
     }
   }
 
---
+++ src/main/java/org/folio/inventory/dataimport/util/MappingConstants.java
@@ -0,0 +1,10 @@
+package org.folio.inventory.dataimport.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public final class MappingConstants {
+
+  public static final String MARC_BIB_RECORD_TYPE = "marc-bib";
+  public static final String MARC_BIB_RECORD_FORMAT = "MARC_BIB";
+}
