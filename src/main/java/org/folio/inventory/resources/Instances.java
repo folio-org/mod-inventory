e04c5e013c0a3933059b995012815765cf6880aa
Andrei Bordak <Andrei_Bordak@epam.com>
2025-02-12 10:58:25 +0400

    MODINVSTOR-1138: Adjust mark-deleted endpoint behavior to set the "deleted" markers

--- src/main/java/org/folio/inventory/resources/Instances.java
+++ src/main/java/org/folio/inventory/resources/Instances.java
@@ -343,6 +343,7 @@ public class Instances extends AbstractInstances {
   private void updateVisibility(Instance instance, RoutingContext routingContext,  InstanceCollection instanceCollection) {
     instance.setDiscoverySuppress(true);
     instance.setStaffSuppress(true);
+    instance.setDeleted(true);
     instanceCollection.update(instance, v -> {
       log.info("staffSuppress and discoverySuppress properties are set to true for instance {}",
         instance.getId());
