9aff96192e6215cab7193232b519b9d994f46480
Roman_Chernetskyi <Roman_Chernetskyi@epam.com>
2025-03-04 16:42:44 +0200

    [MODSOURCE-760] Keep staffSuppressed and discoverySuppressed true if delete leader changed using QuickMarc

--- src/main/java/org/folio/inventory/dataimport/handlers/actions/InstanceUpdateDelegate.java
+++ src/main/java/org/folio/inventory/dataimport/handlers/actions/InstanceUpdateDelegate.java
@@ -51,7 +51,7 @@ public class InstanceUpdateDelegate {
 
       return InstanceUtil.findInstanceById(instanceId, instanceCollection)
         .onSuccess(existingInstance -> fillVersion(existingInstance, eventPayload))
-        .compose(existingInstance -> updateInstance(existingInstance, mappedInstance))
+        .compose(existingInstance -> updateInstance(existingInstance, mappedInstance, marcRecord))
         .compose(updatedInstance -> updateInstanceInStorage(updatedInstance, instanceCollection));
     } catch (Exception e) {
       LOGGER.error("Error updating inventory instance", e);
@@ -91,9 +91,14 @@ public class InstanceUpdateDelegate {
       : JsonObject.mapFrom(parsedRecord.getContent());
   }
 
-  private Future<Instance> updateInstance(Instance existingInstance, org.folio.Instance mappedInstance) {
+  private Future<Instance> updateInstance(Instance existingInstance, org.folio.Instance mappedInstance, Record marcRecord) {
     try {
       mappedInstance.setId(existingInstance.getId());
+      if (marcRecord.getAdditionalInfo().getSuppressDiscovery()) {
+        mappedInstance.withStaffSuppress(true);
+        mappedInstance.withDiscoverySuppress(true);
+      }
+
       JsonObject existing = JsonObject.mapFrom(existingInstance);
       JsonObject mapped = JsonObject.mapFrom(mappedInstance);
       JsonObject mergedInstanceAsJson = InstanceUtil.mergeInstances(existing, mapped);
