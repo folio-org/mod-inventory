2103d699703bc788c4b6b54fa30b193aef95d1cf
psmagin <pavlo_smahin@epam.com>
2025-12-22 11:31:30 +0200

    update tests

--- src/main/java/org/folio/inventory/dataimport/handlers/actions/HoldingsUpdateDelegate.java
+++ src/main/java/org/folio/inventory/dataimport/handlers/actions/HoldingsUpdateDelegate.java
@@ -1,30 +1,20 @@
 package org.folio.inventory.dataimport.handlers.actions;
 
-import static java.lang.String.format;
 import static org.folio.inventory.dataimport.util.LoggerUtil.logParametersUpdateDelegate;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.Map;
-
 import io.vertx.core.Future;
-import io.vertx.core.Promise;
 import io.vertx.core.json.JsonObject;
-
-import org.apache.http.HttpStatus;
+import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import org.folio.Holdings;
 import org.folio.HoldingsRecord;
 import org.folio.inventory.common.Context;
-import org.folio.inventory.dataimport.exceptions.OptimisticLockingException;
 import org.folio.inventory.dataimport.util.HoldingsRecordUtil;
-import org.folio.inventory.domain.HoldingsRecordCollection;
-import org.folio.inventory.exceptions.NotFoundException;
 import org.folio.inventory.services.HoldingsCollectionService;
 import org.folio.inventory.storage.Storage;
-import org.folio.processing.mapping.defaultmapper.RecordMapper;
 import org.folio.processing.mapping.defaultmapper.RecordMapperBuilder;
 import org.folio.processing.mapping.defaultmapper.processor.parameters.MappingParameters;
 import org.folio.rest.jaxrs.model.ParsedRecord;
@@ -37,17 +27,20 @@ public class HoldingsUpdateDelegate {
   private static final String MAPPING_RULES_KEY = "MAPPING_RULES";
   private static final String MAPPING_PARAMS_KEY = "MAPPING_PARAMS";
   private static final String MARC_FORMAT = "MARC_HOLDINGS";
-  private static final String MARC_NAME = "MARC";
+  private static final String SOURCE_NAME = "MARC";
 
-  private final Storage storage;
-  private final HoldingsCollectionService holdingsCollectionService;
   private static final ObjectMapper MAPPER = new ObjectMapper();
 
   static {
     MAPPER.setSerializationInclusion(JsonInclude.Include.ALWAYS);
-    MAPPER.configOverride(HoldingsRecord.class).setInclude(JsonInclude.Value.construct(JsonInclude.Include.ALWAYS, JsonInclude.Include.ALWAYS));
-    MAPPER.configOverride(Holdings.class).setInclude(JsonInclude.Value.construct(JsonInclude.Include.ALWAYS, JsonInclude.Include.ALWAYS));
+    MAPPER.configOverride(HoldingsRecord.class)
+      .setInclude(JsonInclude.Value.construct(JsonInclude.Include.ALWAYS, JsonInclude.Include.ALWAYS));
+    MAPPER.configOverride(Holdings.class)
+      .setInclude(JsonInclude.Value.construct(JsonInclude.Include.ALWAYS, JsonInclude.Include.ALWAYS));
   }
+
+  private final Storage storage;
+  private final HoldingsCollectionService holdingsCollectionService;
 
   public HoldingsUpdateDelegate(Storage storage, HoldingsCollectionService holdingsCollectionService) {
     this.storage = storage;
@@ -57,21 +50,20 @@ public class HoldingsUpdateDelegate {
   public Future<HoldingsRecord> handle(Map<String, String> eventPayload, Record marcRecord, Context context) {
     logParametersUpdateDelegate(LOGGER, eventPayload, marcRecord, context);
     try {
-      JsonObject mappingRules = new JsonObject(eventPayload.get(MAPPING_RULES_KEY));
-      MappingParameters mappingParameters =
-        new JsonObject(eventPayload.get(MAPPING_PARAMS_KEY)).mapTo(MappingParameters.class);
+      var mappingRules = new JsonObject(eventPayload.get(MAPPING_RULES_KEY));
+      var mappingParameters = new JsonObject(eventPayload.get(MAPPING_PARAMS_KEY)).mapTo(MappingParameters.class);
 
-      JsonObject parsedRecord = retrieveParsedContent(marcRecord.getParsedRecord());
-      String holdingsId = marcRecord.getExternalIdsHolder().getHoldingsId();
+      var parsedRecord = retrieveParsedContent(marcRecord.getParsedRecord());
+      var holdingsId = marcRecord.getExternalIdsHolder().getHoldingsId();
       LOGGER.info("Holdings update with holdingId: {}", holdingsId);
-      RecordMapper<Holdings> recordMapper = RecordMapperBuilder.buildMapper(MARC_FORMAT);
+      var recordMapper = RecordMapperBuilder.<Holdings>buildMapper(MARC_FORMAT);
       var mappedHoldings = recordMapper.mapRecord(parsedRecord, mappingParameters, mappingRules);
-      HoldingsRecordCollection holdingsRecordCollection = storage.getHoldingsRecordCollection(context);
+      var holdingsRecordCollection = storage.getHoldingsRecordCollection(context);
 
-      return getHoldingsRecordById(holdingsId, holdingsRecordCollection)
+      return holdingsCollectionService.getById(holdingsId, holdingsRecordCollection)
         .compose(existingHoldingsRecord -> findSourceId(context)
-          .compose(sourceId -> mergeRecords(existingHoldingsRecord, mappedHoldings, sourceId)))
-        .compose(updatedHoldingsRecord -> updateHoldingsRecord(updatedHoldingsRecord, holdingsRecordCollection));
+          .map(sourceId -> mergeRecords(existingHoldingsRecord, mappedHoldings, sourceId)))
+        .compose(modifiedRecord -> holdingsCollectionService.update(modifiedRecord, holdingsRecordCollection));
     } catch (Exception e) {
       LOGGER.error("Error updating inventory holdings", e);
       return Future.failedFuture(e);
@@ -80,30 +72,11 @@ public class HoldingsUpdateDelegate {
 
   private JsonObject retrieveParsedContent(ParsedRecord parsedRecord) {
     return parsedRecord.getContent() instanceof String
-      ? new JsonObject(parsedRecord.getContent().toString())
-      : JsonObject.mapFrom(parsedRecord.getContent());
+           ? new JsonObject(parsedRecord.getContent().toString())
+           : JsonObject.mapFrom(parsedRecord.getContent());
   }
 
-  private Future<HoldingsRecord> getHoldingsRecordById(String holdingsId,
-                                                       HoldingsRecordCollection holdingsRecordCollection) {
-    Promise<HoldingsRecord> promise = Promise.promise();
-    holdingsRecordCollection.findById(holdingsId, success -> {
-        if (success.getResult() == null) {
-          LOGGER.error("Can't find Holdings by id: {} ", holdingsId);
-          promise.fail(new NotFoundException(format("Can't find Holdings by id: %s ", holdingsId)));
-        } else {
-          promise.complete(success.getResult());
-        }
-      },
-      failure -> {
-        LOGGER.error(format("Error retrieving Holdings by id %s - %s, status code %s", holdingsId, failure.getReason(),
-          failure.getStatusCode()));
-        promise.fail(failure.getReason());
-      });
-    return promise.future();
-  }
-
-  private Future<HoldingsRecord> mergeRecords(HoldingsRecord existingRecord, Holdings mappedRecord, String sourceId) {
+  private HoldingsRecord mergeRecords(HoldingsRecord existingRecord, Holdings mappedRecord, String sourceId) {
     try {
       mappedRecord.setId(existingRecord.getId());
       mappedRecord.setVersion(existingRecord.getVersion());
@@ -111,31 +84,14 @@ public class HoldingsUpdateDelegate {
       var existing = new JsonObject(MAPPER.writeValueAsString(existingRecord));
       var mapped = new JsonObject(MAPPER.writeValueAsString(mappedRecord));
       var merged = HoldingsRecordUtil.mergeHoldingsRecords(existing, mapped);
-      var mergedHoldingsRecord = merged.mapTo(HoldingsRecord.class);
-      return Future.succeededFuture(mergedHoldingsRecord);
+      return merged.mapTo(HoldingsRecord.class);
     } catch (Exception e) {
-      LOGGER.error("Error updating holdings", e);
-      return Future.failedFuture(e);
+      throw new IllegalStateException("Error updating holdings", e);
     }
-  }
-
-  private Future<HoldingsRecord> updateHoldingsRecord(HoldingsRecord holdingsRecord,
-                                                      HoldingsRecordCollection holdingsRecordCollection) {
-    Promise<HoldingsRecord> promise = Promise.promise();
-    holdingsRecordCollection.update(holdingsRecord, success -> promise.complete(holdingsRecord),
-      failure -> {
-        if (failure.getStatusCode() == HttpStatus.SC_CONFLICT) {
-          promise.fail(new OptimisticLockingException(failure.getReason()));
-        } else {
-          LOGGER.error(format("Error updating Holdings - %s, status code %s", failure.getReason(), failure.getStatusCode()));
-          promise.fail(failure.getReason());
-        }
-      });
-    return promise.future();
   }
 
   private Future<String> findSourceId(Context context) {
     var sourceCollection = storage.getHoldingsRecordsSourceCollection(context);
-    return holdingsCollectionService.findSourceIdByName(sourceCollection, MARC_NAME);
+    return holdingsCollectionService.findSourceIdByName(sourceCollection, SOURCE_NAME);
   }
 }
--- src/test/java/org/folio/inventory/eventhandlers/UpdateHoldingsQuickMarcEventHandlerTest.java
+++ src/test/java/org/folio/inventory/eventhandlers/UpdateHoldingsQuickMarcEventHandlerTest.java
@@ -2,24 +2,27 @@ package org.folio.inventory.eventhandlers;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import io.vertx.core.Future;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
-import java.util.function.Consumer;
-
-import io.vertx.core.Future;
-import io.vertx.core.json.Json;
-import io.vertx.core.json.JsonObject;
-
+import org.folio.HoldingsRecord;
 import org.folio.HoldingsType;
+import org.folio.inventory.TestUtil;
+import org.folio.inventory.common.Context;
+import org.folio.inventory.dataimport.handlers.actions.HoldingsUpdateDelegate;
+import org.folio.inventory.dataimport.handlers.quickmarc.UpdateHoldingsQuickMarcEventHandler;
+import org.folio.inventory.domain.HoldingsRecordCollection;
 import org.folio.inventory.domain.HoldingsRecordsSourceCollection;
 import org.folio.inventory.services.HoldingsCollectionService;
+import org.folio.inventory.storage.Storage;
 import org.folio.processing.mapping.defaultmapper.processor.parameters.MappingParameters;
 import org.junit.Assert;
 import org.junit.Before;
@@ -30,24 +33,16 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import org.folio.HoldingsRecord;
-import org.folio.inventory.TestUtil;
-import org.folio.inventory.common.Context;
-import org.folio.inventory.common.domain.Failure;
-import org.folio.inventory.common.domain.Success;
-import org.folio.inventory.dataimport.handlers.actions.HoldingsUpdateDelegate;
-import org.folio.inventory.dataimport.handlers.quickmarc.UpdateHoldingsQuickMarcEventHandler;
-import org.folio.inventory.domain.HoldingsRecordCollection;
-import org.folio.inventory.storage.Storage;
-
 @RunWith(MockitoJUnitRunner.class)
 public class UpdateHoldingsQuickMarcEventHandlerTest {
 
   private static final String MAPPING_RULES_PATH = "src/test/resources/handlers/holdings-rules.json";
   private static final String INSTANCE_PATH = "src/test/resources/handlers/holdings.json";
   private static final String RECORD_PATH = "src/test/resources/handlers/holdings-record.json";
-  private static final String HOLDINGS_WITH_CELL_NUMBER_PATH = "src/test/resources/handlers/holdings-with-call-number.json";
-  private static final String HOLDINGS_RECORD_WITHOUT_CELL_NUMBER_PATH = "src/test/resources/handlers/holdings-record-without-call-number.json";
+  private static final String HOLDINGS_WITH_CELL_NUMBER_PATH =
+    "src/test/resources/handlers/holdings-with-call-number.json";
+  private static final String HOLDINGS_RECORD_WITHOUT_CELL_NUMBER_PATH =
+    "src/test/resources/handlers/holdings-record-without-call-number.json";
   private static final String HOLDINGS_ID = "65cb2bf0-d4c2-4886-8ad0-b76f1ba75d61";
   private static final String HOLDINGS_TYPE_ID = "fe19bae4-da28-472b-be90-d442e2428eadx";
 
@@ -66,40 +61,29 @@ public class UpdateHoldingsQuickMarcEventHandlerTest {
   private HoldingsUpdateDelegate holdingsUpdateDelegate;
   private JsonObject mappingRules;
   private JsonObject holdingsRecordJson;
-  private HoldingsRecord existingHoldingsRecord;
-  private HoldingsRecord existingHoldingsRecordWithCallNumber;
 
   @Before
   public void setUp() throws IOException {
-    existingHoldingsRecord = new JsonObject(TestUtil.readFileFromPath(INSTANCE_PATH)).mapTo(HoldingsRecord.class);
-    existingHoldingsRecordWithCallNumber = new JsonObject(TestUtil.readFileFromPath(
-      HOLDINGS_WITH_CELL_NUMBER_PATH)).mapTo(HoldingsRecord.class);
+    var existingHoldingsRecord =
+      new JsonObject(TestUtil.readFileFromPath(INSTANCE_PATH)).mapTo(HoldingsRecord.class);
+    var existingHoldingsRecordWithCallNumber =
+      new JsonObject(TestUtil.readFileFromPath(HOLDINGS_WITH_CELL_NUMBER_PATH)).mapTo(HoldingsRecord.class);
     holdingsUpdateDelegate = Mockito.spy(new HoldingsUpdateDelegate(storage, holdingsCollectionService));
     updateHoldingsQuickMarcEventHandler = new UpdateHoldingsQuickMarcEventHandler(holdingsUpdateDelegate, context);
 
     var sourceId = String.valueOf(UUID.randomUUID());
     when(storage.getHoldingsRecordCollection(any(Context.class))).thenReturn(holdingsRecordCollection);
     when(storage.getHoldingsRecordsSourceCollection(any(Context.class))).thenReturn(sourceCollection);
-    when(holdingsCollectionService.findSourceIdByName(any(HoldingsRecordsSourceCollection.class), any())).thenReturn(Future.succeededFuture(sourceId));
-
-    doAnswer(invocationOnMock -> {
-      Consumer<Success<HoldingsRecord>> successHandler = invocationOnMock.getArgument(1);
-      successHandler.accept(new Success<>(existingHoldingsRecord));
-      return null;
-    }).when(holdingsRecordCollection).findById(eq("ddd266ef-07ac-4117-be13-d418b8cd6902"), any(), any());
-
-    doAnswer(invocationOnMock -> {
-      Consumer<Success<HoldingsRecord>> successHandler = invocationOnMock.getArgument(1);
-      successHandler.accept(new Success<>(existingHoldingsRecordWithCallNumber));
-      return null;
-    }).when(holdingsRecordCollection).findById(eq("abc266ef-07ac-4117-be13-d418b8cd6902"), any(), any());
-
-    doAnswer(invocationOnMock -> {
+    when(holdingsCollectionService.findSourceIdByName(any(HoldingsRecordsSourceCollection.class), any())).thenReturn(
+      Future.succeededFuture(sourceId));
+    when(holdingsCollectionService.getById(eq("ddd266ef-07ac-4117-be13-d418b8cd6902"), any())).thenReturn(
+      Future.succeededFuture(existingHoldingsRecord));
+    when(holdingsCollectionService.getById(eq("abc266ef-07ac-4117-be13-d418b8cd6902"), any())).thenReturn(
+      Future.succeededFuture(existingHoldingsRecordWithCallNumber));
+    when(holdingsCollectionService.update(any(), any())).thenAnswer(invocationOnMock -> {
       HoldingsRecord holdingsRecord = invocationOnMock.getArgument(0);
-      Consumer<Success<HoldingsRecord>> successHandler = invocationOnMock.getArgument(1);
-      successHandler.accept(new Success<>(holdingsRecord));
-      return null;
-    }).when(holdingsRecordCollection).update(any(), any(), any());
+      return Future.succeededFuture(holdingsRecord);
+    });
 
     when(context.getTenantId()).thenReturn("dummy");
     when(context.getToken()).thenReturn("token");
@@ -192,12 +176,8 @@ public class UpdateHoldingsQuickMarcEventHandlerTest {
 
   @Test
   public void shouldSendError() {
-    doAnswer(invocationOnMock -> {
-      Consumer<Failure> failureHandler = invocationOnMock.getArgument(2);
-      failureHandler.accept(new Failure("Internal Server Error", 500));
-      return null;
-    }).when(holdingsRecordCollection).update(any(), any(), any());
-
+    when(holdingsCollectionService.update(any(), any())).thenReturn(
+      Future.failedFuture(new RuntimeException("Internal Server Error")));
 
     List<HoldingsType> holdings = new ArrayList<>();
     holdings.add(new HoldingsType().withName("testingnote$a").withId(HOLDINGS_TYPE_ID));
