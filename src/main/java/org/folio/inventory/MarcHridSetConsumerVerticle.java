45e30bf317c9f2ffcdaa7da37ac2ba49ecc8bf0a
Ruslan Lavrov <ruslan_lavrov@epam.com>
2026-04-10 19:25:45 +0300

    Removed unused MarcBibInstanceHridSetKafkaHandler handler

--- src/main/java/org/folio/inventory/MarcHridSetConsumerVerticle.java
+++ src/main/java/org/folio/inventory/MarcHridSetConsumerVerticle.java
@@ -1,17 +1,13 @@
 package org.folio.inventory;
 
-import static org.folio.DataImportEventTypes.DI_SRS_MARC_BIB_INSTANCE_HRID_SET;
 import static org.folio.DataImportEventTypes.DI_SRS_MARC_HOLDINGS_HOLDING_HRID_SET;
 import static org.folio.inventory.dataimport.util.ConsumerWrapperUtil.constructModuleName;
 
-import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.folio.inventory.dataimport.consumers.MarcBibInstanceHridSetKafkaHandler;
 import org.folio.inventory.dataimport.consumers.MarcHoldingsRecordHridSetKafkaHandler;
 import org.folio.inventory.dataimport.handlers.actions.HoldingsUpdateDelegate;
-import org.folio.inventory.dataimport.handlers.actions.InstanceUpdateDelegate;
 import org.folio.inventory.services.HoldingsCollectionService;
 import org.folio.inventory.support.KafkaConsumerVerticle;
 
@@ -22,20 +18,13 @@ public class MarcHridSetConsumerVerticle extends KafkaConsumerVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) {
-    var marcBibConsumerWrapper = createConsumer(DI_SRS_MARC_BIB_INSTANCE_HRID_SET.value(), BASE_PROPERTY);
     var marcHoldingsConsumerWrapper = createConsumer(DI_SRS_MARC_HOLDINGS_HOLDING_HRID_SET.value(), BASE_PROPERTY);
-
     var holdingsCollectionService = new HoldingsCollectionService();
-    var instanceUpdateDelegate = new InstanceUpdateDelegate(getStorage());
     var holdingsRecordUpdateDelegate = new HoldingsUpdateDelegate(getStorage(), holdingsCollectionService);
 
-    var marcBibInstanceHridSetKafkaHandler = new MarcBibInstanceHridSetKafkaHandler(instanceUpdateDelegate, getMappingMetadataCache());
     var marcHoldingsRecordHridSetKafkaHandler = new MarcHoldingsRecordHridSetKafkaHandler(holdingsRecordUpdateDelegate, getMappingMetadataCache());
 
-    Future.all(
-        marcBibConsumerWrapper.start(marcBibInstanceHridSetKafkaHandler, constructModuleName()),
-        marcHoldingsConsumerWrapper.start(marcHoldingsRecordHridSetKafkaHandler, constructModuleName())
-      )
+    marcHoldingsConsumerWrapper.start(marcHoldingsRecordHridSetKafkaHandler, constructModuleName())
       .onFailure(startPromise::fail)
       .onSuccess(ar -> startPromise.complete());
   }
