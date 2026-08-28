4775457b23337cd2f7210e4318c2f50ef084fd7a
Pavlo Smahin <pavlo_smahin@epam.com>
2026-08-28 11:33:57 +0300

    - cleanup oudated scripts

--- register-managed.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-tenant_id=${1:-demo_tenant}
-okapi_proxy_address=${2:-http://localhost:9130}
-
-./okapi-registration/managed-deployment/register.sh \
-  ${okapi_proxy_address} \
-  ${tenant_id}
-
--- stop-managed-demo.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-okapi_proxy_address=${1:-http://localhost:9130}
-tenant_id="demo_tenant"
-
-./okapi-registration/managed-deployment/unregister.sh \
-  ${okapi_proxy_address} \
-  ${tenant_id}
